bb9275c773caeb18964a74c037fdbf3274942fd4
shisyamo4131 <sevenstar_1226@hotmail.com>
2024-06-26 11:48:35 +0900

    commit

--- pages/developments/sandbox.vue
+++ pages/developments/sandbox.vue
@@ -1,18 +1,9 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="6">
-        <g-customer-sites customer-id="eD2htQ5XV6btjfgvMd0R" />
-      </v-col>
-    </v-row>
-  </v-container>
+  <div>sandbox</div>
 </template>
 
 <script>
-import GCustomerSites from '~/components/organisms/GCustomerSitesCard.vue'
-export default {
-  components: { GCustomerSites },
-}
+export default {}
 </script>
 
 <style></style>
