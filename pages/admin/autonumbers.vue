a88cfe2d87c3909945aaed904d1f14e3d474b8fb
shisyamo4131 <sevenstar_1226@hotmail.com>
2024-10-01 16:44:20 +0900

    commit

--- pages/admin/autonumbers.vue
+++ pages/admin/autonumbers.vue
@@ -1,9 +1,11 @@
 <script>
 import GBtnRegistIcon from '~/components/atoms/btns/GBtnRegistIcon.vue'
-import GTextFieldSearch from '~/components/molecules/inputs/GTextFieldSearch.vue'
-import GDataTable from '~/components/atoms/tables/GDataTable.vue'
-import GCardSubmitCancel from '~/components/molecules/cards/GCardSubmitCancel.vue'
 import GInputAutonumber from '~/components/molecules/inputs/GInputAutonumber.vue'
+import Autonumber from '~/models/Autonumber'
+import GEditModeMixin from '~/mixins/GEditModeMixin'
+import GTemplateIndex from '~/components/templates/GTemplateIndex.vue'
+import GDialogInput from '~/components/molecules/dialogs/GDialogInput.vue'
+import GDataTableAutonumbers from '~/components/molecules/tables/GDataTableAutonumbers.vue'
 export default {
   /***************************************************************************
    * NAME
@@ -13,29 +15,25 @@ export default {
    * COMPONENTS
    ***************************************************************************/
   components: {
-    GTextFieldSearch,
     GBtnRegistIcon,
-    GDataTable,
-    GCardSubmitCancel,
     GInputAutonumber,
+    GTemplateIndex,
+    GDialogInput,
+    GDataTableAutonumbers,
   },
   /***************************************************************************
-   * DATA
+   * MIXINS
    ***************************************************************************/
-  asyncData({ app }) {
-    const model = app.$Autonumber()
-    const items = model.subscribe()
-    return { model, items }
-  },
+  mixins: [GEditModeMixin],
   /***************************************************************************
    * DATA
    ***************************************************************************/
   data() {
     return {
       dialog: false,
-      editMode: 'REGIST',
-      loading: false,
-      search: null,
+      instance: new Autonumber(),
+      items: [],
+      listener: new Autonumber(),
     }
   },
   /***************************************************************************
@@ -47,89 +45,65 @@ export default {
    ***************************************************************************/
   watch: {
     dialog(v) {
-      v || this.initialize()
+      if (!v) {
+        this.instance.initialize()
+        this.editMode = this.CREATE
+      }
     },
+  },
+  /***************************************************************************
+   * MOUNTED
+   ***************************************************************************/
+  mounted() {
+    this.items = this.listener.subscribeDocs()
   },
   /***************************************************************************
    * DESTROYED
    ***************************************************************************/
   destroyed() {
-    this.model.unsubscribe()
+    this.listener.unsubscribe()
   },
   /***************************************************************************
    * METHODS
    ***************************************************************************/
   methods: {
-    initialize() {
-      this.model.initialize()
-      this.editMode = 'REGIST'
-    },
-    onClickEdit(item) {
-      this.model.initialize(item)
-      this.editMode = 'UPDATE'
+    onClickRow(item) {
+      // 詳細ページが出来上がったらこちらを適用
+      // this.$router.push(`/customers/${item.docId}`)
+      this.instance.initialize(item)
+      this.editMode = this.UPDATE
       this.dialog = true
-    },
-    onClickDelete(item) {
-      this.model.initialize(item)
-      this.editMode = 'DELETE'
-      this.dialog = true
-    },
-    async submit() {
-      this.loading = true
-      try {
-        if (this.editMode === 'REGIST') await this.model.create()
-        if (this.editMode === 'UPDATE') await this.model.update()
-        if (this.editMode === 'DELETE') await this.model.delete()
-        this.dialog = false
-      } catch (err) {
-        // eslint-disable-next-line
-        console.error(err)
-        alert(err.message)
-      } finally {
-        this.loading = false
-      }
     },
   },
 }
 </script>
 
 <template>
-  <div>
-    <v-toolbar flat :color="$vuetify.theme.themes.light.background">
-      <g-text-field-search v-model="search" />
-      <v-dialog v-model="dialog" max-width="600" persistent scrollable>
+  <g-template-index :items="items">
+    <template #append-search>
+      <g-dialog-input v-model="dialog">
         <template #activator="{ attrs, on }">
           <g-btn-regist-icon color="primary" v-bind="attrs" v-on="on" />
         </template>
-        <g-card-submit-cancel
-          :dialog.sync="dialog"
-          label="自動採番"
-          :edit-mode="editMode"
-          :loading="loading"
-          @click:submit="submit"
-        >
-          <g-input-autonumber v-bind.sync="model" :edit-mode="editMode" />
-        </g-card-submit-cancel>
-      </v-dialog>
-    </v-toolbar>
-    <v-container fluid>
-      <g-data-table
-        :actions="['edit', 'delete']"
-        :headers="[
-          { text: 'コレクション名', value: 'collectionId' },
-          { text: '現在値', value: 'current' },
-          { text: '桁数', value: 'length' },
-          { text: 'フィールド名', value: 'field' },
-          { text: '状態', value: 'status' },
-        ]"
-        :items="items"
-        sort-by="collectionId"
-        @click:edit="onClickEdit"
-        @click:delete="onClickDelete"
-      >
-      </g-data-table>
-    </v-container>
-  </div>
+        <template #default="{ attrs, on }">
+          <g-input-autonumber
+            v-bind="attrs"
+            :edit-mode="editMode"
+            :instance="instance"
+            v-on="on"
+          />
+        </template>
+      </g-dialog-input>
+    </template>
+    <template #default="{ attrs, on, search }">
+      <g-data-table-autonumbers
+        v-bind="attrs"
+        :search="search"
+        @click:row="onClickRow"
+        v-on="on"
+      />
+    </template>
+  </g-template-index>
 </template>
 
 <style></style>
