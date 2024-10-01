589353e5bfaeaa0d3418b90b6dff136555c802aa
shisyamo4131 <sevenstar_1226@hotmail.com>
2024-10-01 16:25:12 +0900

    Created.

---
+++ functions/triggers/outsourcers.js
@@ -0,0 +1,116 @@
+/**
+ * ## outsourcers.js
+ *
+ * Outsourcersドキュメントの作成・更新・削除トリガーに関する処理です。
+ *
+ * ### 従属ドキュメントの同期削除について
+ * 従属ドキュメントの同期削除は以下の理由で行いません。
+ * - アプリの仕様上、従属するドキュメントが存在する場合、親ドキュメントは削除できません。
+ * - ドキュメントの整合性を保つために従属するドキュメントも削除することを検討しましたが、
+ *   アプリで実装している論理削除の設計を崩壊させてしまう為に取りやめました。
+ */
+import {
+  onDocumentCreated,
+  onDocumentDeleted,
+  onDocumentUpdated,
+} from 'firebase-functions/v2/firestore'
+import { logger } from 'firebase-functions/v2'
+import { isDocumentChanged } from '../modules/utils.js'
+import Outsourcer from '../models/Outsourcer.js'
+
+/****************************************************************************
+ * ドキュメントが作成されたときにトリガーされる関数。
+ * - 作成されたドキュメントのデータを元に、Realtime Databaseにインデックスを作成します。
+ *
+ * @author shisyamo4131
+ * @version 1.0.0
+ ****************************************************************************/
+export const onCreate = onDocumentCreated(
+  'Outsourcers/{docId}',
+  async (event) => {
+    const docId = event.params.docId
+
+    // ログに更新されたドキュメントIDを含める
+    logger.info(
+      `Outsourcersドキュメントが更新されました。ドキュメントID: ${event.params.docId}`
+    )
+
+    try {
+      // Realtime Databaseにインデックスを作成
+      await Outsourcer.syncIndex(docId)
+    } catch (err) {
+      // エラーハンドリング
+      logger.error(
+        `Outsourcersドキュメントの同期処理中にエラーが発生しました: ${err.message}`,
+        { docId: event.params.docId }
+      )
+      throw err
+    }
+  }
+)
+
+/****************************************************************************
+ * Outsourcerドキュメントの更新トリガーです。
+ * - インデックスを更新します。
+ * - 従属するドキュメントの outsourcer プロパティを同期します。
+ *
+ * #### 注意事項
+ * - ドキュメントの内容に変更があったかどうかは`isDocumentChanged()`を利用します。
+ *
+ * @author shisyamo4131
+ * @version 1.0.0
+ ****************************************************************************/
+export const onUpdate = onDocumentUpdated(
+  'Outsourcers/{docId}',
+  async (event) => {
+    try {
+      // ドキュメントに変更がなければ処理を終了
+      if (!isDocumentChanged(event)) return
+
+      // ログに更新されたドキュメントIDを含める
+      logger.info(
+        `Outsourcersドキュメントが更新されました。ドキュメントID: ${event.params.docId}`
+      )
+      // Realtime Databaseにインデックスを更新
+      await Outsourcer.syncIndex(event.params.docId)
+    } catch (err) {
+      // エラーハンドリング
+      logger.error(
+        `Outsourcersドキュメントの同期処理中にエラーが発生しました: ${err.message}`,
+        { docId: event.params.docId }
+      )
+      throw err
+    }
+  }
+)
+
+/****************************************************************************
+ * ドキュメントが削除されたときにトリガーされる関数。
+ * - インデックスを削除します。
+ *
+ * @author shisyamo4131
+ * @version 1.0.0
+ ****************************************************************************/
+export const onDelete = onDocumentDeleted(
+  'Outsourcers/{docId}',
+  async (event) => {
+    const docId = event.params.docId
+
+    // ログに更新されたドキュメントIDを含める
+    logger.info(
+      `Outsourcersドキュメントが削除されました。ドキュメントID: ${event.params.docId}`
+    )
+
+    try {
+      // Realtime Databaseインデックスを削除
+      await Outsourcer.syncIndex(docId, true)
+    } catch (err) {
+      // エラーハンドリング
+      logger.error(
+        `Outsourcersドキュメントの同期処理中にエラーが発生しました: ${err.message}`,
+        { docId: event.params.docId }
+      )
+      throw err
+    }
+  }
+)
