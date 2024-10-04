0c6b35ebc40978e9dad066889ea2151b9cd46130
shisyamo4131 <sevenstar_1226@hotmail.com>
2024-10-04 13:21:36 +0900

    New component.

---
+++ components/atoms/chips/GChipEmployeeStatus.vue
@@ -0,0 +1,44 @@
+<script>
+/**
+ * ## GChipEmployeeStatus
+ *
+ * - 従業員の状態を表すChipコンポーネントです。
+ *
+ * @author shisyamo4131
+ * @version 1.0.0
+ * @updates
+ * - version 1.0.0 - 2024-10-04 - 初版作成
+ */
+
+export default {
+  /***************************************************************************
+   * PROPS
+   ***************************************************************************/
+  props: {
+    value: {
+      type: String,
+      validator: (v) => ['active', 'expired'].includes(v),
+      required: true,
+    },
+  },
+
+  /***************************************************************************
+   * COMPUTED
+   ***************************************************************************/
+  computed: {
+    label() {
+      if (!this.value) return 'undefined'
+      if (this.value === 'active') return '在職中'
+      return '退職'
+    },
+  },
+}
+</script>
+
+<template>
+  <v-chip v-bind="$attrs" v-on="$listeners">
+    {{ label }}
+  </v-chip>
+</template>
+
+<style></style>
