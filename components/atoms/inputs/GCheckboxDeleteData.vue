4d702a2f30cc0c6c5e0e81c743fc48f23fcab56d
shisyamo4131 <sevenstar_1226@hotmail.com>
2024-10-04 13:46:06 +0900

    New component.

---
+++ components/atoms/inputs/GCheckboxDeleteData.vue
@@ -0,0 +1,42 @@
+<script>
+/**
+ * ## GCheckboxDeleteData
+ *
+ * - ユーザーからデータの削除指示を受け付けるためのCheckboxコンポーネントです。
+ *
+ * @author shisyamo4131
+ * @version 1.0.0
+ * @updates
+ * - version 1.0.0 - 2024-10-04 - 初版作成
+ */
+
+import GCheckbox from './GCheckbox.vue'
+
+export default {
+  /***************************************************************************
+   * COMPONENTS
+   ***************************************************************************/
+  components: { GCheckbox },
+
+  /***************************************************************************
+   * MODEL
+   ***************************************************************************/
+  model: {
+    prop: 'input-value',
+    event: 'change',
+  },
+
+  /***************************************************************************
+   * PROPS
+   ***************************************************************************/
+  props: {
+    label: { type: String, default: 'このデータを削除する', required: false },
+  },
+}
+</script>
+
+<template>
+  <g-checkbox v-bind="$attrs" :label="label" v-on="$listeners" />
+</template>
+
+<style></style>
