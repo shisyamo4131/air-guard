84b41a256793d85564cd8aa1472e26002833ba12
shisyamo4131 <sevenstar_1226@hotmail.com>
2024-10-14 14:11:17 +0900

    New component.

---
+++ components/atoms/chips/GChipDayType.vue
@@ -0,0 +1,48 @@
+<script>
+/**
+ * @author shisyamo4131
+ * @version 1.0.0
+ * @updates
+ * - version 1.0.0 - 2024-10-14 - 初版作成
+ */
+
+export default {
+  /***************************************************************************
+   * PROPS
+   ***************************************************************************/
+  props: {
+    value: {
+      type: String,
+      default: 'undefined',
+      validator: (v) =>
+        [
+          'scheduled',
+          'non-statutory-holiday',
+          'legal-holiday',
+          'undefined',
+        ].includes(v),
+      required: false,
+    },
+  },
+
+  /***************************************************************************
+   * COMPUTED
+   ***************************************************************************/
+  computed: {
+    color() {
+      return this.$DAY_TYPE[this.value]?.color || undefined
+    },
+    label() {
+      return this.$DAY_TYPE[this.value] || 'error'
+    },
+  },
+}
+</script>
+
+<template>
+  <v-chip v-bind="$attrs" :color="color" dark v-on="$listeners">{{
+    label
+  }}</v-chip>
+</template>
+
+<style></style>
