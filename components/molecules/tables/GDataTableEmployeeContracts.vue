570e66ee3c946f3ab1d5338abb8a773185c4c488
shisyamo4131 <sevenstar_1226@hotmail.com>
2024-10-21 15:15:57 +0900

    upgrade: 現在適用雇用契約の表示

--- components/molecules/tables/GDataTableEmployeeContracts.vue
+++ components/molecules/tables/GDataTableEmployeeContracts.vue
@@ -26,6 +26,7 @@ export default {
      * DataTableに表示する列の定義です。指定しない場合、コンポーネント既定の列が表示されます。
      */
     headers: { type: Array, default: () => [], required: false },
+    items: { type: Array, default: () => [], required: false },
     /**
      * DataTableに表示されるitemsの表示順です。規定値は`startDate`です。
      */
@@ -67,6 +68,25 @@ export default {
           ]
       }
     },
+    computedItems() {
+      // 配列を複製して日付の降順にソート
+      const sorted = this.items
+        .slice()
+        .sort((a, b) => new Date(b.startDate) - new Date(a.startDate))
+
+      // 現在日付以下のデータの最初の1件を取得
+      const currentItem = sorted.find(
+        ({ startDate }) => new Date(startDate) <= new Date()
+      )
+
+      // すべてのアイテムに対して isCurrent をセット
+      return sorted.map((item) => {
+        return {
+          ...item,
+          isCurrent: item === currentItem, // currentItem の場合だけ true
+        }
+      })
+    },
   },
 }
 </script>
@@ -74,18 +94,13 @@ export default {
 <template>
   <g-data-table
     v-bind="{ ...$props, ...$attrs, headers: inHeaders }"
+    :items="computedItems"
     :mobile-breakpoint="0"
     v-on="$listeners"
   >
     <!-- `startDate` -->
     <template #[`item.startDate`]="{ item }">
-      <v-icon
-        v-if="$store.getters['employee-contracts/isCurrent'](item.docId)"
-        color="green"
-        left
-        small
-        >mdi-play</v-icon
-      >
+      <v-icon v-if="item.isCurrent" color="green" left small>mdi-play</v-icon>
       {{ item.startDate }}
     </template>
 
