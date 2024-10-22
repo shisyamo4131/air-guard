723ddd61941136a230b49ce7bf67b439944e44cb
shisyamo4131 <sevenstar_1226@hotmail.com>
2024-10-22 19:14:41 +0900

    commit

--- pages/developments/sandbox.vue
+++ pages/developments/sandbox.vue
@@ -1,29 +1,26 @@
 <template>
   <v-container>
-    <div></div>
+    <v-card>
+      <v-btn @click="generatePdf">generate</v-btn>
+    </v-card>
   </v-container>
 </template>
 
 <script>
-import OperationResult from '~/models/OperationResult'
 export default {
-  data() {
-    return {
-      instance: new OperationResult(),
-    }
-  },
-  computed: {},
-  mounted() {
-    this.test()
-  },
   methods: {
-    async test() {
-      await this.instance.fetchDocs([
-        ['where', 'docId', '==', '00wfglbWeVCKXabMPVPr'],
-      ])
+    async generatePdf() {
+      const content = [
+        {
+          text: 'Hello, this is a PDF generated with pdfmake!',
+          fontSize: 16,
+        },
+        { text: 'Nuxt.jsを使っています', fontSize: 12 },
+      ]
+      await this.$generatePdf({ content })
     },
   },
 }
 </script>
 
-<style></style>
+<style scoped></style>
