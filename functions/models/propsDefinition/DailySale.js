4aeaaa04f1a218ea6e635b5ca1916e386100151e
shisyamo4131 <sevenstar_1226@hotmail.com>
2024-10-24 15:26:19 +0900

    change: プロパティと Accessor の追加
    - consumptionTax プロパティを追加
    - month, year, amount, consumptionTax の Accessor を追加

--- functions/models/propsDefinition/DailySale.js
+++ functions/models/propsDefinition/DailySale.js
@@ -1,5 +1,8 @@
 import { generateVueProps, generateClassProps } from './propsUtil.js'
 
+/*****************************************************************************
+ * PROPERTIES
+ *****************************************************************************/
 const propsDefinition = {
   docId: { type: String, default: '', required: false, requiredByClass: false },
   date: { type: String, default: '', required: false, requiredByClass: true },
@@ -21,9 +24,70 @@ const propsDefinition = {
     required: false,
     requiredByClass: false,
   },
+  consumptionTax: {
+    type: Number,
+    default: null,
+    required: false,
+    requiredByClass: false,
+  },
 }
-
 const vueProps = generateVueProps(propsDefinition)
 const classProps = generateClassProps(propsDefinition)
 
-export { vueProps, classProps }
+/*****************************************************************************
+ * ACCESSOR
+ *****************************************************************************/
+const month = {
+  configurable: true,
+  enumerable: true,
+  get() {
+    if (!this.date) return ''
+    return this.date.slice(0, 7)
+  },
+  set(v) {},
+}
+
+const year = {
+  configurable: true,
+  enumerable: true,
+  get() {
+    if (!this.month) return
+    return this.month.slice(0, 4)
+  },
+  set(v) {},
+}
+
+const amount = {
+  configurable: true,
+  enumerable: true,
+  get() {
+    const result = { operationResults: 0 }
+    result.operationResults = this.operationResults.reduce(
+      (sum, i) => sum + i.sales.total,
+      0
+    )
+    return result
+  },
+  set(v) {},
+}
+
+const consumptionTax = {
+  configurable: true,
+  enumerable: true,
+  get() {
+    return this.operationResults.reduce(
+      (sum, i) => sum + i.consumptionTax.amount,
+      0
+    )
+  },
+  set(v) {},
+}
+
+const accessor = {
+  amount,
+  consumptionTax,
+  month,
+  year,
+}
+
+export { vueProps, classProps, accessor }
