1c17b1d2fd28045b7127ea38f2def5b85083d464
shisyamo4131 <sevenstar_1226@hotmail.com>
2024-10-25 18:35:03 +0900

    add: New Component.
    - 配置管理用の従業員カードコンポーネント
    - 今後修正が入る予定

---
+++ components/molecules/cards/GCardEmployeeArrangement.vue
@@ -0,0 +1,50 @@
+<script>
+/**
+ * ## GCardEmployeeArrangement
+ *
+ * - 配置管理で使用する従業員のカードです。
+ *
+ * @author shisyamo4131
+ */
+export default {
+  /***************************************************************************
+   * PROPS
+   ***************************************************************************/
+  props: {
+    employeeId: { type: String, required: true },
+    arrangement: { type: Object, required: true },
+  },
+
+  /***************************************************************************
+   * COMPUTED
+   ***************************************************************************/
+  computed: {
+    employee() {
+      return this.$store.getters['employees/get'](this.employeeId)
+    },
+  },
+}
+</script>
+
+<template>
+  <v-card v-bind="$attrs" v-on="$listeners">
+    <v-card-text class="pa-2">
+      <div class="d-flex">
+        <h4>{{ employee.abbr }}</h4>
+        <v-spacer />
+        <v-icon left small>mdi-pencil</v-icon>
+        <v-icon small @click="$emit('click:remove', employeeId)">
+          mdi-close
+        </v-icon>
+      </div>
+      <div class="d-flex align-center">
+        <v-icon left small>mdi-clock-outline</v-icon>
+        <div>{{ arrangement?.startTime || null }}</div>
+        <div>-</div>
+        <div>{{ arrangement?.endTime || null }}</div>
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<style></style>
