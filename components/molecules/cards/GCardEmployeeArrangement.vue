bd43596ca8d1deef6adaddbc8c33949f2ab74191
shisyamo4131 <sevenstar_1226@hotmail.com>
2024-10-29 15:34:49 +0900

    add: 配置管理で使用する従業員のカードコンポーネント

--- components/molecules/cards/GCardEmployeeArrangement.vue
+++ components/molecules/cards/GCardEmployeeArrangement.vue
@@ -11,8 +11,26 @@ export default {
    * PROPS
    ***************************************************************************/
   props: {
-    employeeId: { type: String, required: true },
+    /**
+     * 当該従業員の配置情報
+     */
     arrangement: { type: Object, required: true },
+    /**
+     * Card を省略表示にします。
+     */
+    ellipsis: { type: Boolean, default: false, required: false },
+    /**
+     * 対象の従業員ID
+     */
+    employeeId: { type: String, required: true },
+    /**
+     * true にすると従業員名の先頭に連勤アイコンを表示します。
+     */
+    showContinuous: { type: Boolean, default: false, required: false },
+    /**
+     * true にすると従業員名の先頭にエラーアイコンを表示します。
+     */
+    showError: { type: Boolean, default: false, required: false },
   },
 
   /***************************************************************************
@@ -30,6 +48,12 @@ export default {
   <v-card v-bind="$attrs" v-on="$listeners">
     <v-card-text class="pa-2">
       <div class="d-flex">
+        <v-icon v-if="showError" left small color="error">
+          mdi-alert-circle
+        </v-icon>
+        <v-icon v-else-if="showContinuous" left small color="warning">
+          mdi-star
+        </v-icon>
         <h4>{{ employee.abbr }}</h4>
         <v-spacer />
         <v-icon left small>mdi-pencil</v-icon>
@@ -37,11 +61,13 @@ export default {
           mdi-close
         </v-icon>
       </div>
-      <div class="d-flex align-center">
-        <v-icon left small>mdi-clock-outline</v-icon>
-        <div>{{ arrangement?.startTime || null }}</div>
-        <div>-</div>
-        <div>{{ arrangement?.endTime || null }}</div>
+      <div v-show="!ellipsis">
+        <div class="d-flex align-center">
+          <v-icon left small>mdi-clock-outline</v-icon>
+          <div>{{ arrangement?.startTime || null }}</div>
+          <div>-</div>
+          <div>{{ arrangement?.endTime || null }}</div>
+        </div>
       </div>
     </v-card-text>
   </v-card>
