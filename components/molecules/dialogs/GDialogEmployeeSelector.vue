45c42f143bc969158ea024a08596221801cdd2ac
shisyamo4131 <sevenstar_1226@hotmail.com>
2024-10-31 15:28:59 +0900

    change:
    - コメントを整理
    - VDialog の maxWidth を初期値ありで props に定義
    - ダイアログの終了時に GChipGroupKanaFileter の initialize() を実行するように機能追加
    - props.value を実装し、activator スロット外からも起動できるように修正
    - モバイルでフルスクリーン表示
    - 退職者を含めるための GSwitch の場所を変更

--- components/molecules/dialogs/GDialogEmployeeSelector.vue
+++ components/molecules/dialogs/GDialogEmployeeSelector.vue
@@ -1,24 +1,24 @@
 <script>
+/**
+ * ## GDialogEmployeeSelector
+ *
+ * 機能詳細:
+ * - 従業員を選択するためのダイアログコンポーネントです。
+ * - 従業員を選択するだけのコンポーネントであるため、GDataTable で従業員名のみを表示します。
+ *   -> GDataTableEmployees を使用していません。
+ * - 選択可能な従業員データは親コンポーネントから `items` で受け取ります。
+ * - 従業員の `fullNameKan` での絞り込みが可能です。
+ * - 既定では在職中の従業員のみが一覧表示されます。switch の切り替えで退職者も含めることができます。
+ *
+ * @author shisyamo4131
+ */
+
 import GBtnCancelIcon from '~/components/atoms/btns/GBtnCancelIcon.vue'
 import GBtnSubmitIcon from '~/components/atoms/btns/GBtnSubmitIcon.vue'
 import GChipEmployeeStatus from '~/components/atoms/chips/GChipEmployeeStatus.vue'
 import GChipGroupKanaFilter from '~/components/atoms/chips/GChipGroupKanaFilter.vue'
 import GSwitch from '~/components/atoms/inputs/GSwitch.vue'
 import GDataTable from '~/components/atoms/tables/GDataTable.vue'
-
-/**
- * ## GDialogEmployeeSelector
- *
- * - 従業員を選択するためのダイアログコンポーネントです。
- * - 選択可能な従業員データは親コンポーネントから `items` で受け取ります。
- * - 従業員の `fullNameKan` での絞り込みが可能です。
- * - 既定では在職中の従業員のみが一覧表示されます。switch の切り替えで退職者も含めることができます。
- *
- * @author shisyamo4131
- * @version 1.0.0
- * @updates
- * - version 1.0.0 - 2024-10-04 - 初版作成
- */
 
 export default {
   /***************************************************************************
@@ -38,6 +38,8 @@ export default {
    ***************************************************************************/
   props: {
     items: { type: Array, default: () => [], required: false },
+    maxWidth: { type: [String, Number], default: 360, required: false },
+    value: { type: Boolean, default: false, required: false },
   },
 
   /***************************************************************************
@@ -56,9 +58,18 @@ export default {
    * COMPUTED
    ***************************************************************************/
   computed: {
+    /**
+     * GDataTable の headers プロパティにバインドされる配列を返します。
+     * - 従業員を選択するコンポーネントであるため、カラムは従業員名（略称）に固定されます。
+     */
     headers() {
       return [{ text: '従業員名', value: 'abbr' }]
     },
+    /**
+     * GDataTableEmployees の items プロパティにバインドされる配列を返します。
+     * - props.items から GChipGroupKanaFilter で選択された正規表現に該当するものを抽出します。
+     * - data.includeExpired が false の場合、status が active であるものを抽出します。
+     */
     filteredItems() {
       return structuredClone(this.items)
         .filter(({ fullNameKana, status }) => {
@@ -74,9 +85,28 @@ export default {
    * WATCHERS
    ***************************************************************************/
   watch: {
+    /**
+     * data.dialog を監視します。
+     * - 値が false に更新されたら data.selectedItems を初期化します。
+     * - 値が false に更新されたら GChipGroupKanaFileter の initialize() を実行します。
+     * - 値を input イベントで emit します。
+     */
     dialog(v) {
-      if (v) return
-      this.selectedItems.splice(0)
+      if (!v) {
+        this.selectedItems.splice(0)
+        this.$refs.filter.initialize()
+      }
+      this.$emit('input', v)
+    },
+    /**
+     * props.value を監視します。
+     * - 値を data.dialog に同期します。
+     */
+    value: {
+      handler(v) {
+        this.dialog = v
+      },
+      immediate: true,
     },
   },
 
@@ -84,9 +114,18 @@ export default {
    * METHODS
    ***************************************************************************/
   methods: {
+    /**
+     * cancel ボタンがクリックされた時の処理です。
+     * - data.dialog を false に更新します。
+     */
     onClickCancel() {
       this.dialog = false
     },
+    /**
+     * submit ボタンがクリックされた時の処理です。
+     * - data.selectedItems の内容を複製して click:submit イベントで emit します。
+     * - data.dialog を false に更新します。
+     */
     onClickSubmit() {
       this.$emit('click:submit', structuredClone(this.selectedItems))
       this.dialog = false
@@ -96,16 +135,32 @@ export default {
 </script>
 
 <template>
-  <v-dialog v-model="dialog" scrollable max-width="360">
+  <v-dialog
+    v-bind="$attrs"
+    v-model="dialog"
+    :fullscreen="$vuetify.breakpoint.mobile"
+    scrollable
+    v-on="$listeners"
+  >
     <template #activator="{ attrs, on }">
       <slot name="activator" v-bind="{ attrs, on }" />
     </template>
-    <v-card>
+    <v-card :tile="$vuetify.breakpoint.mobile">
       <v-card-title class="g-card__title">従業員選択</v-card-title>
+      <v-divider />
+      <v-container class="d-flex justify-end">
+        <g-switch
+          v-model="includeExpired"
+          class="mt-0"
+          label="退職者を含める"
+          hide-details
+        />
+      </v-container>
       <v-divider />
       <v-card-text class="pa-0 d-flex flex-grow-1" style="height: 480px">
         <v-container style="width: 68px">
           <g-chip-group-kana-filter
+            ref="filter"
             :chip-options="{ small: true, label: true }"
             column
             :regex.sync="regex"
@@ -133,15 +188,6 @@ export default {
           </template>
         </g-data-table>
       </v-card-text>
-      <v-divider />
-      <v-container class="d-flex justify-end">
-        <g-switch
-          v-model="includeExpired"
-          class="mt-0"
-          label="退職者を含める"
-          hide-details
-        />
-      </v-container>
       <v-divider />
       <v-card-actions class="justify-space-between">
         <g-btn-cancel-icon @click="onClickCancel" />
