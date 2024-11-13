bee79128735fbcd2837781c5857e756efd5b5853
shisyamo4131 <sevenstar_1226@hotmail.com>
2024-11-13 14:04:55 +0900

    add: 配置管理表従業員（外注先）カードコンポーネント（Ver.2）
    ‐ 確認モード（mode: confirmation）で、配置状態を切り替えられる機能を実装
    - 外枠の色が配置状態に応じて変更
    ‐ 外注先はカードの色ではなく、名前の色を赤にするように変更

---
+++ components/organisms/placements/GPlacementEmployeeCardV2.vue
@@ -0,0 +1,183 @@
+<script>
+/**
+ * EmployeeCard component for placements.
+ * This component displays an employee card with optional icons for continuous service or error.
+ * It includes a start and end time, and options to edit or remove the card.
+ *
+ * - emits 'click:edit' event when edit-button is clicked.
+ * - emits 'click:remove' event when edit-button is clicked.
+ * - 状態チップをクリックすると click:status イベントが emit されます。
+ */
+
+export default {
+  /***************************************************************************
+   * PROPS
+   ***************************************************************************/
+  props: {
+    // Display the text in an abbreviated format if true
+    ellipsis: { type: Boolean, default: false },
+    // Target Employee ID
+    employeeId: { type: String, required: true },
+    // End time in HH:MM format
+    endTime: { type: String, default: '' },
+    // Show an icon at the beginning of the name if the service is continuous
+    showContinuous: { type: Boolean, default: false },
+    // Show an error icon at the beginning of the employee name if true
+    showError: { type: Boolean, default: false },
+    // 存在状態にするかどうかです
+    showExist: { type: Boolean, default: false },
+    // Start time in HH:MM format
+    startTime: { type: String, default: '' },
+    // 新規入場かどうか
+    isNewEntry: { type: Boolean, default: false },
+
+    /**
+     * チップの表示モードを切り替えます。
+     * placement: 配置モードです。移動のためのアイコンと編集のためのボタンが表示されます。
+     * confirmation: 確認モードです。配置確認、上番・下番などの切り替えボタンが表示されます。
+     */
+    mode: {
+      type: String,
+      default: 'placement',
+      validator: (mode) => ['placement', 'confirmation'].includes(mode),
+      required: false,
+    },
+
+    confirmedAt: { type: String, default: undefined, required: false },
+    arrivedAt: { type: String, default: undefined, required: false },
+    leavedAt: { type: String, default: undefined, required: false },
+  },
+
+  /***************************************************************************
+   * COMPUTED
+   ***************************************************************************/
+  computed: {
+    /**
+     * コンポーネントの外枠の色を定義する css を返します。
+     * - props.showError が true の場合、外枠の色は赤になります。
+     * - それ以外の場合は props.status に応じた色になります。
+     */
+    cardStyle() {
+      const color = this.showError
+        ? 'red'
+        : this.color
+        ? getComputedStyle(document.documentElement)
+            .getPropertyValue(`--v-${this.color}-base`)
+            .trim()
+        : 'lightgrey'
+      return {
+        border: `1px solid ${color}`,
+      }
+    },
+
+    /**
+     * 状態チップ、Cardコンポーネントの外枠の色です。
+     */
+    color() {
+      if (this.status === 'confirmed') return 'info'
+      if (this.status === 'arrived') return 'primary'
+      if (this.status === 'leaved') return 'secondary'
+      return undefined
+    },
+
+    status() {
+      if (!this.confirmedAt) return 'unconfirmed'
+      if (!this.arrivedAt) return 'confirmed'
+      if (!this.leavedAt) return 'arrived'
+      return 'leaved'
+    },
+
+    /**
+     * 状態チップのラベルです。
+     */
+    statusLabel() {
+      if (this.status === 'confirmed') return '確認済'
+      if (this.status === 'arrived') return '上番済'
+      if (this.status === 'leaved') return '下番済'
+      return '未確認'
+    },
+
+    /**
+     * Retrieve the employee object from Vuex using the employee ID.
+     */
+    employee() {
+      return this.$store.getters['employees/get'](this.employeeId) || {}
+    },
+  },
+}
+</script>
+
+<template>
+  <v-card
+    v-bind="$attrs"
+    :color="showExist ? 'red' : undefined"
+    :style="cardStyle"
+    v-on="$listeners"
+  >
+    <v-card-text :class="ellipsis ? 'pa-1' : 'pa-2'">
+      <div class="d-flex">
+        <div>
+          <div class="d-flex">
+            <!-- 削除ボタン -->
+            <v-btn class="mr-1" icon x-small @click="$emit('click:remove')">
+              <v-icon small>mdi-close</v-icon>
+            </v-btn>
+
+            <!-- エラーアイコン -->
+            <v-icon v-if="showError" left small color="error">
+              mdi-alert-circle
+            </v-icon>
+
+            <!-- 連勤アイコン -->
+            <v-icon v-else-if="showContinuous" left small color="warning">
+              mdi-star
+            </v-icon>
+
+            <!-- 名前 -->
+            <h4>{{ employee?.abbr || 'N/A' }}</h4>
+
+            <!-- 新規アイコン -->
+            <v-icon v-if="isNewEntry" right small color="error"
+              >mdi-new-box</v-icon
+            >
+          </div>
+
+          <!-- 開始時刻, 終了時刻, 編集ボタン -->
+          <div v-show="!ellipsis" class="d-flex flex-grow-1 align-center">
+            <v-icon left small>mdi-clock-outline</v-icon>
+            <div>{{ startTime }}</div>
+            <div>-</div>
+            <div>{{ endTime }}</div>
+            <v-btn
+              v-if="mode === 'placement'"
+              class="ml-1"
+              icon
+              x-small
+              @click="$emit('click:edit')"
+            >
+              <v-icon small>mdi-pencil</v-icon>
+            </v-btn>
+          </div>
+        </div>
+
+        <!-- ハンドルアイコン -->
+        <v-icon v-if="mode === 'placement'" class="handle ml-auto">
+          mdi-arrow-all
+        </v-icon>
+
+        <!-- 状態チップ -->
+        <v-chip
+          v-else
+          class="ml-auto align-self-center"
+          :color="color"
+          x-small
+          @click="$emit('click:status', { employeeId, status })"
+        >
+          {{ statusLabel }}
+        </v-chip>
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<style></style>
---
+++ components/organisms/placements/GPlacementOutsourcerCardV2.vue
@@ -0,0 +1,162 @@
+<script>
+/**
+ * OutsourcerCard component for placements.
+ * This component displays an outsourcer card with optional icons for continuous service or error.
+ * It includes a start and end time, and options to edit or remove the card.
+ *
+ * - emits 'click:edit' event when edit-button is clicked.
+ * - emits 'click:remove' event when edit-button is clicked.
+ * - 状態チップをクリックすると click:status イベントが emit されます。
+ */
+
+export default {
+  /***************************************************************************
+   * PROPS
+   ***************************************************************************/
+  props: {
+    // Display the text in an abbreviated format if true
+    ellipsis: { type: Boolean, default: false },
+    // Target Outsourcer KEY
+    outsourcerKey: { type: String, required: true },
+    // End time in HH:MM format
+    endTime: { type: String, default: '' },
+    // Start time in HH:MM format
+    startTime: { type: String, default: '' },
+
+    /**
+     * チップの表示モードを切り替えます。
+     * placement: 配置モードです。移動のためのアイコンと編集のためのボタンが表示されます。
+     * confirmation: 確認モードです。配置確認、上番・下番などの切り替えボタンが表示されます。
+     */
+    mode: {
+      type: String,
+      default: 'placement',
+      validator: (mode) => ['placement', 'confirmation'].includes(mode),
+      required: false,
+    },
+
+    confirmedAt: { type: String, default: undefined, required: false },
+    arrivedAt: { type: String, default: undefined, required: false },
+    leavedAt: { type: String, default: undefined, required: false },
+  },
+
+  /***************************************************************************
+   * COMPUTED
+   ***************************************************************************/
+  computed: {
+    /**
+     * コンポーネントの外枠の色を定義する css を返します。
+     * - props.showError が true の場合、外枠の色は赤になります。
+     * - それ以外の場合は props.status に応じた色になります。
+     */
+    cardStyle() {
+      const color = this.showError
+        ? 'red'
+        : this.color
+        ? getComputedStyle(document.documentElement)
+            .getPropertyValue(`--v-${this.color}-base`)
+            .trim()
+        : 'lightgrey'
+      return {
+        border: `1px solid ${color}`,
+      }
+    },
+
+    /**
+     * 状態チップ、Cardコンポーネントの外枠の色です。
+     */
+    color() {
+      if (this.status === 'confirmed') return 'info'
+      if (this.status === 'arrived') return 'primary'
+      if (this.status === 'leaved') return 'secondary'
+      return undefined
+    },
+
+    status() {
+      if (!this.confirmedAt) return 'unconfirmed'
+      if (!this.arrivedAt) return 'confirmed'
+      if (!this.leavedAt) return 'arrived'
+      return 'leaved'
+    },
+
+    /**
+     * 状態チップのラベルです。
+     */
+    statusLabel() {
+      if (this.status === 'confirmed') return '確認済'
+      if (this.status === 'arrived') return '上番済'
+      if (this.status === 'leaved') return '下番済'
+      return '未確認'
+    },
+
+    outsourcerId() {
+      return this.outsourcerKey.split('-')?.[0] || undefined
+    },
+    /**
+     * Retrieve the outsourcer object from Vuex using the outsourcer ID.
+     */
+    outsourcer() {
+      return this.$store.getters['outsourcers/get'](this.outsourcerId) || {}
+    },
+  },
+}
+</script>
+
+<template>
+  <v-card v-bind="$attrs" :style="cardStyle" v-on="$listeners">
+    <v-card-text :class="ellipsis ? 'pa-1' : 'pa-2'">
+      <div class="d-flex">
+        <div>
+          <div class="d-flex">
+            <!-- 削除ボタン -->
+            <v-btn icon x-small @click="$emit('click:remove')">
+              <v-icon small>mdi-close</v-icon>
+            </v-btn>
+
+            <!-- 名前 -->
+            <h4 class="text-truncate red--text" style="max-width: 120px">
+              {{ outsourcer?.abbr || 'N/A' }}
+            </h4>
+          </div>
+
+          <!-- 開始時刻, 終了時刻, 編集ボタン -->
+          <div v-show="!ellipsis" class="d-flex flex-grow-1 align-center">
+            <v-icon left small>mdi-clock-outline</v-icon>
+            <div>{{ startTime }}</div>
+            <div>-</div>
+            <div>{{ endTime }}</div>
+            <v-btn
+              v-if="mode === 'placement'"
+              class="ml-1"
+              icon
+              x-small
+              @click="$emit('click:edit')"
+            >
+              <v-icon small>mdi-pencil</v-icon>
+            </v-btn>
+          </div>
+        </div>
+
+        <!-- ハンドルアイコン -->
+        <v-icon v-if="mode === 'placement'" class="handle ml-auto">
+          mdi-arrow-all
+        </v-icon>
+
+        <!-- 状態チップ -->
+        <v-chip
+          v-else
+          class="ml-auto align-self-center"
+          :color="color"
+          x-small
+          @click="$emit('click:status', { outsourcerKey, status })"
+        >
+          {{ statusLabel }}
+        </v-chip>
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<style>
+/* Add custom styling here as needed */
+</style>
