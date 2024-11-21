92e179a5c6d387ac0c360901214cb88d496e383e
shisyamo4131 <sevenstar_1226@hotmail.com>
2024-11-21 16:16:13 +0900

    change:
    - GCardDraggableSiteOrder の実装によるリファクタリング

--- components/organisms/placements/GPlacementDraggableSiteOrder.vue
+++ components/organisms/placements/GPlacementDraggableSiteOrder.vue
@@ -1,11 +1,10 @@
 <script>
-import draggable from 'vuedraggable'
-import GBtnCancelIcon from '~/components/atoms/btns/GBtnCancelIcon.vue'
-import GBtnSubmitIcon from '~/components/atoms/btns/GBtnSubmitIcon.vue'
-import GChipWorkShift from '~/components/atoms/chips/GChipWorkShift.vue'
+import GCardDraggableSiteOrder from '~/components/molecules/cards/GCardDraggableSiteOrder.vue'
 
 export default {
-  components: { draggable, GBtnSubmitIcon, GBtnCancelIcon, GChipWorkShift },
+  components: {
+    GCardDraggableSiteOrder,
+  },
   props: {
     // v-model との連携用
     value: { type: Boolean, default: false },
@@ -39,10 +38,10 @@ export default {
   },
   methods: {
     // サイト並べ替えの保存処理
-    async onClickSubmit() {
+    async onClickSubmit(newOrder) {
       this.loading = true
       try {
-        await this.$store.dispatch('site-order/update', this.siteOrder)
+        await this.$store.dispatch('site-order/update', newOrder)
         this.dialog = false
       } catch (err) {
         // eslint-disable-next-line
@@ -66,49 +65,12 @@ export default {
     <template #activator="props">
       <slot name="activator" v-bind="props" />
     </template>
-    <v-card class="d-flex flex-column" :tile="$vuetify.breakpoint.mobile">
-      <v-toolbar class="flex-grow-0" color="secondary" dark dense flat>
-        <v-icon left>mdi-swap-vertical</v-icon>
-        <v-toolbar-title>現場並べ替え</v-toolbar-title>
-        <v-spacer />
-        <g-btn-cancel-icon :disabled="loading" @click="dialog = false" />
-      </v-toolbar>
-      <v-card-text class="px-0 px-md-6">
-        <draggable
-          v-model="siteOrder"
-          tag="v-list"
-          handle=".handle"
-          :disabled="loading"
-          v-bind="{ animation: 300 }"
-        >
-          <div v-for="(order, index) in siteOrder" :key="index">
-            <v-list-item>
-              <v-list-item-content>
-                <v-list-item-title>
-                  <g-chip-work-shift :value="order.workShift" small />
-                  {{
-                    $store.getters['site-order/site'](order.siteId)?.name ||
-                    'N/A'
-                  }}
-                </v-list-item-title>
-              </v-list-item-content>
-              <v-list-item-icon>
-                <v-icon class="handle">mdi-arrow-all</v-icon>
-              </v-list-item-icon>
-            </v-list-item>
-            <v-divider />
-          </div>
-        </draggable>
-      </v-card-text>
-      <v-card-actions class="justify-end">
-        <g-btn-submit-icon
-          color="primary"
-          :disabled="loading"
-          :loading="loading"
-          @click="onClickSubmit"
-        />
-      </v-card-actions>
-    </v-card>
+    <g-card-draggable-site-order
+      :loading="loading"
+      :site-order="siteOrder"
+      @click:close="dialog = false"
+      @click:submit="onClickSubmit"
+    />
   </v-dialog>
 </template>
 
