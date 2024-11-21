72a26a41455f6955e21316451d72498e18786c49
shisyamo4131 <sevenstar_1226@hotmail.com>
2024-11-21 16:02:32 +0900

    add: 新規実装

---
+++ components/atoms/icons/GIconUpDown.vue
@@ -0,0 +1,19 @@
+<script>
+/**
+ * 上下を表すアイコンコンポーネントです。
+ *
+ * - 主にドラッグによる並べ替えができることをユーザーに通知するために使用します。
+ *
+ * @author shisyamo4131
+ */
+
+export default {}
+</script>
+
+<template>
+  <v-icon v-bind="$attrs" v-on="$listeners">mdi-arrow-up-down-bold</v-icon>
+</template>
+
+<style>
+/* 必要に応じてスタイルを追加 */
+</style>
