f45fa079ca57a488b363f87ca7cc84e880a41faf
shisyamo4131 <sevenstar_1226@hotmail.com>
2024-12-03 11:15:53 +0900

    fixed: 稼働実績ドキュメントに対する hasMany の指定の誤りを修正

--- models/Employee.js
+++ models/Employee.js
@@ -30,7 +30,7 @@ export default class Employee extends FireModel {
     },
     {
       collection: 'OperationResults',
-      field: 'employeeId',
+      field: 'employeeIds',
       condition: 'array-contains',
       type: 'collection',
     },
