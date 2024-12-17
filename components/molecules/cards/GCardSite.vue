54abe2ff7f85c31042cf4ea85924893e78154105
shisyamo4131 <sevenstar_1226@hotmail.com>
2024-12-17 16:39:43 +0900

    refact: 不要なファイルを削除

--- components/molecules/cards/GCardSite.vue
+++ components/molecules/cards/GCardSite.vue
@@ -1,24 +1,99 @@
 <script>
-import Site from '~/models/Site'
 /**
- * ### GSiteCard
- *
- * 現場情報を表示・編集するためのCardコンポーネントです。
- *
+ * 現場情報を表示・編集する Card コンポーネントです。
+ * - props.docId を指定すると現場ドキュメントを読み込んで表示します。
+ * - props.instance に現場インスタンスを渡すことでも表示が可能です。
+ * - props.docId は props.instance よりも優先されます。
+ * - props.docId が指定されている場合のみ編集が可能です。
+ *   -> インスタンスが Minimal などであった場合に情報が欠落してしまう為
  * @author shisyamo4131
- * @version 1.0.0
- * @updates
- * - version 1.0.0 - 2024-07-12 - 初版作成
  */
+import GDialogInput from '../dialogs/GDialogInput.vue'
+import GInputSite from '../inputs/GInputSite.vue'
+import GBtnEditIcon from '~/components/atoms/btns/GBtnEditIcon.vue'
+import Site from '~/models/Site'
 export default {
+  components: { GDialogInput, GBtnEditIcon, GInputSite },
   /***************************************************************************
    * PROPS
    ***************************************************************************/
   props: {
+    /**
+     * 当該現場を編集不可能にします。
+     */
+    disableEdit: { type: Boolean, default: false, required: false },
+
+    /**
+     * 現場ドキュメントID
+     * - 指定された ID に該当するドキュメントを Firestore から取得します。
+     * - ID が指定された場合 props.instance は無視されます。
+     */
+    docId: { type: String, default: undefined, required: false },
+
+    /**
+     * 現場インスタンスを受け取ります。
+     */
     instance: {
       type: Object,
-      required: true,
+      default: () => new Site(),
       validator: (v) => v instanceof Site,
+      required: false,
+    },
+  },
+
+  /****************************************************************************
+   * DATA
+   ****************************************************************************/
+  data() {
+    return {
+      editModel: new Site(),
+      error: {
+        message: null,
+        snackbar: false,
+      },
+      loading: false,
+      snackbar: true,
+    }
+  },
+
+  /****************************************************************************
+   * WATCH
+   ****************************************************************************/
+  watch: {
+    /**
+     * 指定された現場 ID のドキュメントの購読を開始します。
+     */
+    docId: {
+      handler(v) {
+        if (!v) return
+        this.editModel.subscribe(v)
+      },
+      immediate: true,
+    },
+
+    /**
+     * props.instance の内容で data.editModel を初期化します。
+     * - props.docId が指定されている場合は無視されます。
+     */
+    instance: {
+      handler(v) {
+        if (this.docId) return
+        this.editModel.initialize(v)
+      },
+      immediate: true,
+    },
+  },
+
+  /****************************************************************************
+   * METHODS
+   ****************************************************************************/
+  methods: {
+    /**
+     * エラー状態を初期化します。
+     */
+    initError() {
+      this.error.message = null
+      this.error.snackbar = false
     },
   },
 }
@@ -26,26 +101,45 @@ export default {
 
 <template>
   <v-card v-bind="$attrs" v-on="$listeners">
-    <v-card-title class="g-card__title text-truncate d-block"
-      >{{ instance.name }}
-      <v-chip
-        v-if="instance.status === 'expired'"
-        color="red"
-        label
-        x-small
-        outlined
-      >
-        {{ `${$SITE_STATUS[instance.status]}` }}
-      </v-chip>
+    <v-card-title class="g-card__title flex-nowrap">
+      <div class="text-truncate" style="width: 100%">
+        <span>{{ editModel.name }}</span>
+        <v-chip
+          v-if="editModel.status === 'expired'"
+          class="flex-shrink-0"
+          color="red"
+          label
+          x-small
+          outlined
+        >
+          {{ `${$SITE_STATUS[editModel.status]}` }}
+        </v-chip>
+      </div>
+      <g-dialog-input edit-mode="UPDATE">
+        <template #activator="{ attrs, on }">
+          <g-btn-edit-icon
+            v-if="!error.message && docId && !disableEdit"
+            class="ml-auto"
+            v-bind="attrs"
+            color="primary"
+            v-on="on"
+          />
+        </template>
+        <template #default="{ attrs, on }">
+          <g-input-site v-bind="attrs" :instance="editModel" v-on="on" />
+        </template>
+      </g-dialog-input>
     </v-card-title>
     <v-card-subtitle>
-      {{ instance.abbr }}
+      <div style="width: 100%" class="text-truncate">
+        {{ editModel.abbr }}
+      </div>
     </v-card-subtitle>
     <v-card-text>
       <v-chip-group column>
         <v-chip color="primary" label outlined
           ><v-icon left>mdi-domain</v-icon
-          >{{ instance.customer?.abbr || '' }}</v-chip
+          >{{ editModel.customer?.abbr || '' }}</v-chip
         >
       </v-chip-group>
     </v-card-text>
--- pages/developments/sandbox.vue
+++ pages/developments/sandbox.vue
@@ -1,43 +1,16 @@
 <template>
-  <div>
-    <button @click="generatePdf">PDFを生成</button>
-  </div>
+  <v-container>
+    <v-card>
+      <v-card-text>
+        <g-card-site doc-id="03cGBW4Qwon1KJyBupSc" />
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
+import GCardSite from '~/components/molecules/cards/GCardSite.vue'
 export default {
-  methods: {
-    generatePdf() {
-      const data = {
-        name: '金子 真樹',
-        postalCode: '120-0013',
-        address: '東京都足立区弘道一丁目21番11号 コーポすずき302',
-        contractStartDate: '令和06年11月01日',
-        employmentType: '正社員',
-        jobDescription:
-          '警備業務および補助的業務、並びにそれらに付帯する一切の業務',
-        salaryType: '月給',
-        basicSalary: '246,000',
-        positionAllowance: '85,000',
-        qualificationAllowance: '20,000',
-        familyAllowance: '0',
-        bonus: '有',
-        startTime: '8:00',
-        endTime: '17:00',
-        breakTime: '60分',
-        legalHoliday: '日',
-        nonLegalHoliday: '土',
-        healthInsurance: '加入',
-        pension: '加入',
-        employmentInsurance: '加入',
-        remarks: '令和６年１１月１日より、家族手当の支給を停止します。',
-        companyName: '株式会社　唯心',
-        representativeName: '丸山 大三',
-        companyAddress: '東京都台東区清川１－２３－１',
-      }
-
-      this.$GENERATE_LABOR_TERMS(data, { download: false })
-    },
-  },
+  components: { GCardSite },
 }
 </script>
