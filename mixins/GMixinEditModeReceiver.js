28ca6eafdeab7a78ca77da941b9fc012ce8895d3
shisyamo4131 <sevenstar_1226@hotmail.com>
2024-12-17 18:59:11 +0900

    change: computedEditMode を実装

--- mixins/GMixinEditModeReceiver.js
+++ mixins/GMixinEditModeReceiver.js
@@ -2,6 +2,9 @@ import { EDIT_MODES } from '~/utils/EDIT_MODES'
 /**
  * コンポーネントが editMode を受け付けるようにするミックスインです。
  * - 編集モードを示す props.editMode が実装されます。必須で受け付けます。
+ * - このミックスインを利用するコンポーネントでは、computedEditMode を利用することで
+ *   親コンポーネントが editMode に .sync 修飾子を使用することが可能になります。
+ *
  * @author shisyamo4131
  */
 export default {
@@ -15,10 +18,41 @@ export default {
       required: true,
     },
   },
+
+  /***************************************************************************
+   * DATA
+   ***************************************************************************/
+  data() {
+    return {
+      /**
+       * コンポーネント内部で管理する editMode です。
+       */
+      internalEditMode: null,
+    }
+  },
+
   /***************************************************************************
    * COMPUTED
    ***************************************************************************/
   computed: {
+    /**
+     * data.interanalEditMode の値を返します。
+     * このプロパティに値をセットすると、新しい値をペイロードとして
+     * `update:editMode` イベント を emit します。
+     * 当該ミックスインを利用するコンポーネントにおいて editMode の参照を
+     * このプロパティにすることで、親コンポーネントが editMode に .sync 修飾子を
+     * 利用可能になります。
+     */
+    computedEditMode: {
+      get() {
+        return this.internalEditMode
+      },
+      set(v) {
+        this.internalEditMode = v
+        this.$emit('update:editMode', v)
+      },
+    },
+
     CREATE() {
       return EDIT_MODES.CREATE
     },
@@ -27,6 +61,21 @@ export default {
     },
     DELETE() {
       return EDIT_MODES.DELETE
+    },
+  },
+
+  /***************************************************************************
+   * WATCH
+   ***************************************************************************/
+  watch: {
+    /**
+     * props.editMode を監視し、data.internalEditMode と同期します。
+     */
+    editMode: {
+      handler(v) {
+        this.internalEditMode = v
+      },
+      immediate: true,
     },
   },
 }
