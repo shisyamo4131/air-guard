e809f7962abd41d6e32296bd8c96e0d05269db36
shisyamo4131 <sevenstar_1226@hotmail.com>
2024-12-30 15:59:07 +0900

    change:
    - ドキュメントの一括作成処理を関数に分離
    - beforeCreate, beforeUpdate をリファクト

--- models/SiteOperationSchedule.js
+++ models/SiteOperationSchedule.js
@@ -1,32 +1,11 @@
-/**
- * ### SiteOperationSchedule.js
- *
- * #### 概要
- * 現場の稼働予定を管理するためのモデルクラスです。
- *
- * #### 機能詳細:
- * - 同一日、同一勤務区分でのドキュメントは作成できません。（keyにはsiteIdを含みますが、サブコレクションなので日付と勤務区分でのValidationです）
- *
- * #### 注意事項:
- * - ドキュメントのkeyとなるsiteId、date、workShiftは変更できないようにすべきです。
- *   -> Cloud Functions側も含めてsiteId、date、workShiftの変更時にエラーが出ないようにはしていますが、
- *      特にdate、workShiftです。これらが変更されるとCloud Functionsで稼働予定を作り直していますが、
- *      更新履歴が「削除」と「作成」になってしまいます。
- *      また、削除の更新履歴には削除理由を持たせることができません。
- *      稼働予定の「削除」は誤って作成してしまったときなどに行い、キャンセルによって稼働がなくなった場合は
- *      0人で更新すべきです。
- *
- * @author shisyamo4131
- * @version 1.0.0
- *
- * @updates
- * - version 1.0.0 - 2024-09-16 - 初版作成
- */
-
 import { runTransaction } from 'firebase/firestore'
 import { FireModel, firestore } from 'air-firebase'
 import { classProps } from './propsDefinition/SiteOperationSchedule'
 
+/**
+ * 現場の稼働予定を管理するためのモデルクラスです。
+ * @author shisyamo4131
+ */
 export default class SiteOperationSchedule extends FireModel {
   /****************************************************************************
    * STATIC
@@ -35,40 +14,24 @@ export default class SiteOperationSchedule extends FireModel {
   static classProps = classProps
 
   /****************************************************************************
-   * CONSTRUCTOR
-   ****************************************************************************/
-  constructor(item = {}) {
-    super(item)
-  }
-
-  /****************************************************************************
    * beforeCreateをオーバーライドします。
    * - 同一の現場、日付、勤務区分での稼働予定が存在する場合は作成不可です。
    * @returns {Promise<void>} - 処理が完了すると解決されるPromise
    ****************************************************************************/
-  async beforeCreate() {
-    if (!this.siteId) {
-      throw new Error('現場の指定が必要です。')
-    }
-    if (!this.date) {
-      throw new Error('日付の指定が必要です。')
-    }
-    if (!this.workShift) {
-      throw new Error('勤務区分の指定が必要です。')
-    }
-    if (this.isClosed) this.requiredWorkers = 0
-    try {
-      const id = `${this.siteId}-${this.date}-${this.workShift}`
-      const existingSchedule = await this.fetchDoc(id)
-      if (existingSchedule) {
-        throw new Error('同一の稼働予定が既に登録されています。')
+  beforeCreate() {
+    return new Promise((resolve, reject) => {
+      if (!this.siteId) {
+        reject(new Error('現場の指定が必要です。'))
       }
-      await super.beforeCreate()
-    } catch (err) {
-      // eslint-disable-next-line
-      console.error(`[beforeCreate] An error has occured: ${err.message}`)
-      throw err
-    }
+      if (!this.date) {
+        reject(new Error('日付の指定が必要です。'))
+      }
+      if (!this.workShift) {
+        reject(new Error('勤務区分の指定が必要です。'))
+      }
+      if (this.isClosed) this.requiredWorkers = 0
+      resolve()
+    })
   }
 
   /****************************************************************************
@@ -77,51 +40,106 @@ export default class SiteOperationSchedule extends FireModel {
    * @returns {Promise<void>} - 成功すると解決されるPromise
    * @throws {Error} - 現場、日付、勤務区分が変更されている場合にエラーをスローします。
    ****************************************************************************/
-  async beforeUpdate() {
-    // 正規表現を使用して、siteId, date, workShiftを抽出
-    const match = this.docId.match(/^([^-]+)-(\d{4}-\d{2}-\d{2})-([^-]+)$/)
-    const [, siteId, date, workShift] = match
+  beforeUpdate() {
+    return new Promise((resolve, reject) => {
+      // 正規表現を使用して、siteId, date, workShiftを抽出
+      const match = this.docId.match(/^([^-]+)-(\d{4}-\d{2}-\d{2})-([^-]+)$/)
+      const [, siteId, date, workShift] = match
 
-    if (
-      siteId !== this.siteId ||
-      date !== this.date ||
-      workShift !== this.workShift
-    ) {
-      throw new Error('現場、日付、勤務区分は変更できません。')
-    }
+      if (
+        siteId !== this.siteId ||
+        date !== this.date ||
+        workShift !== this.workShift
+      ) {
+        reject(new Error('現場、日付、勤務区分は変更できません。'))
+      }
 
-    if (this.isClosed) this.requiredWorkers = 0
-
-    // 親クラスの beforeUpdate メソッドを呼び出す
-    await super.beforeUpdate()
+      if (this.isClosed) this.requiredWorkers = 0
+      resolve()
+    })
   }
 
   /****************************************************************************
    * createメソッドをオーバーライドします。
-   * - ドキュメントの作成時は、`dates`プロパティに格納されているすべての日付について一括で作成します。
    * - `docId`は`${siteId}-${date}-${workShift}`に固定されます。
-   * - 生成した`docId`を使用して親クラスのcreateメソッドを呼び出します。
-   * - 親クラスのcreateメソッドのtransactionオプションを利用して、`dates`に指定された日数分のドキュメントを作成します。
+   * - bulk オプションが true の場合は `dates` プロパティに保存されているすべての日付について一括で作成します。（既定値: true）
+   * - bulk オプションが false の場合は親クラスのcreateメソッドを呼び出します。
+   * @param {Object} options
+   * @param {boolean} [options.bulk=true] - true の場合、複数のドキュメントを一括作成します。
+   * @param {Object} [options.transaction=null] - Firestore のトランザクションオブジェクト（オプション）
    * @returns {Promise<void>} - 処理が完了すると解決されるPromise
    * @throws {Error} ドキュメント作成中にエラーが発生した場合にエラーをスローします
    ****************************************************************************/
-  async create() {
-    if (!this.dates.length) {
-      throw new Error('[create] 日付が選択されていません。')
-    }
+  async create({ bulk = true, transaction = null } = {}) {
     try {
-      await runTransaction(firestore, async (transaction) => {
-        for (const date of this.dates) {
-          const docId = `${this.siteId}-${date}-${this.workShift}`
-          this.date = date
-          await super.create({ docId, transaction })
+      if (bulk) {
+        await this._bulkCreate({ transaction })
+      } else {
+        // docId を固定
+        const docId = `${this.siteId}-${this.date}-${this.workShift}`
+
+        // ドキュメントが既に存在する場合はエラー
+        const isExist = await this.fetchDoc(docId)
+        if (isExist) {
+          const message = `[create] 既に存在する現場稼働予定です。`
+          throw new Error(message)
         }
-      })
+
+        await super.create({ docId, transaction })
+      }
     } catch (err) {
+      const message = `[create] 現場稼働予定の作成処理でエラーが発生しました。: ${err.message}`
       // eslint-disable-next-line no-console
-      console.error(`[create] Failed to create document: ${err.message}`, {
-        err,
-      })
+      console.error(message, { err })
+      throw err
+    }
+  }
+
+  /****************************************************************************
+   * [PRIVATE]
+   * dates プロパティを参照し、指定された複数（または単一）の日付の現場稼働予定ドキュメントを作成します。
+   * @param {Object} options
+   * @param {Object} [options.transaction=null] - Firestore のトランザクションオブジェクト（オプション）
+   ****************************************************************************/
+  async _bulkCreate({ transaction = null } = {}) {
+    // 一括作成用である dates プロパティをチェック
+    if (!Array.isArray(this.dates) || !this.dates.length) {
+      throw new Error('[_bulkCreate] 日付が選択されていません。')
+    }
+
+    // 作成するドキュメントの ID を配列に用意
+    const docIds = this.dates.map((date) => {
+      return `${this.siteId}-${date}-${this.workShift}`
+    })
+
+    // 既に存在するドキュメントがある場合はエラー
+    const existDocuments = await Promise.all(
+      docIds.map((docId) => this.fetchDoc(docId))
+    )
+    if (existDocuments.some((doc) => doc)) {
+      const message = `[_bulkCreate] 既に稼働予定が登録されている日付が含まれています。`
+      throw new Error(message, { existDocuments })
+    }
+
+    // トランザクション処理を行う関数
+    const transactionHandler = async (txn) => {
+      for (const date of this.dates) {
+        const docId = `${this.siteId}-${date}-${this.workShift}`
+        this.date = date
+        await super.create({ docId, txn })
+      }
+    }
+
+    try {
+      if (transaction) {
+        await transactionHandler(transaction)
+      } else {
+        await runTransaction(firestore, transactionHandler)
+      }
+    } catch (err) {
+      const message = `[_bulkCreate] 現場稼働予定の一括作成処理でエラーが発生しました。: ${err.message}`
+      // eslint-disable-next-line no-console
+      console.error(message, { err })
       throw err
     }
   }
