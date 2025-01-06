9015a35358bf20032f78334e96e52212978c9521
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-01-06 12:24:48 +0900

    add: エラー表示を行うためのアラートコンポーネント

---
+++ components/atoms/alerts/GAlertError.vue
@@ -0,0 +1,34 @@
+<script>
+/**
+ * エラー表示を行うための Alert コンポーネントです。
+ * @author shisyamo4131
+ */
+export default {
+  /***************************************************************************
+   * PROPS
+   ***************************************************************************/
+  props: {
+    /**
+     * エラーオブジェクトを受け取ります。
+     */
+    value: { type: Object, required: true },
+  },
+
+  /***************************************************************************
+   * COMPUTED
+   ***************************************************************************/
+  computed: {
+    message() {
+      return this.value?.message || null
+    },
+  },
+}
+</script>
+
+<template>
+  <v-alert v-bind="$attrs" type="error" v-on="$listeners">
+    {{ message }}
+  </v-alert>
+</template>
+
+<style></style>
