0f81f67424221f4cb8f8ce123df49980eb3a2a2d
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-01-07 13:37:57 +0900

    refact:
    - 必須プロパティが分かりにくかった他、dialog に対する属性を指定できるように。

--- components/managers/GDocumentManager.vue
+++ components/managers/GDocumentManager.vue
@@ -1,4 +1,10 @@
 <script>
+/**
+ * ADocumentManager を拡張したドキュメント管理用共有コンポーネントです。
+ * ドキュメント編集用のダイアログを内包しています。
+ * @author shisyamo4131
+ */
+import { FireModel } from 'air-firebase'
 import GDialogInput from '../molecules/dialogs/GDialogInput.vue'
 import ADocumentManager from '../atoms/renderless/ADocumentManager.vue'
 export default {
@@ -6,15 +12,43 @@ export default {
    * COMPONENTS
    ***************************************************************************/
   components: { GDialogInput, ADocumentManager },
+
+  /***************************************************************************
+   * PROPS
+   ***************************************************************************/
+  props: {
+    /**
+     * 管理対象のドキュメントIDです。
+     */
+    docId: { type: String, required: true },
+
+    /**
+     * ダイアログコンポーネントに引き渡されるプロパティです。
+     */
+    dialogProps: { type: Object, default: () => {}, required: false },
+
+    /**
+     * 管理対象のインスタンスです。
+     * - FireModel を継承したクラスインスタンスである必要があります。
+     */
+    instance: {
+      type: Object,
+      required: true,
+      validator: (instance) => instance instanceof FireModel,
+    },
+  },
 }
 </script>
 
 <template>
-  <a-document-manager v-bind="$attrs">
+  <a-document-manager :doc-id="docId" :instance="instance">
     <template #default="{ doc, dialog, triggers }">
       <div>
         <slot name="default" v-bind="{ attrs: doc, on: triggers }" />
-        <g-dialog-input v-bind="dialog.attrs" v-on="dialog.on">
+        <g-dialog-input
+          v-bind="{ ...dialog.attrs, ...dialogProps }"
+          v-on="dialog.on"
+        >
           <template #default="editorProps">
             <slot name="editor" v-bind="editorProps" />
           </template>
