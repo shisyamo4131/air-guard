72314c5f1cf16033ece5d0087e9aac1079f0da89
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-01-07 13:23:20 +0900

    add:
    - ドキュメント管理のための共有コンポーネントとして新規実装

---
+++ components/managers/GDocumentManager.vue
@@ -0,0 +1,27 @@
+<script>
+import GDialogInput from '../molecules/dialogs/GDialogInput.vue'
+import ADocumentManager from '../atoms/renderless/ADocumentManager.vue'
+export default {
+  /***************************************************************************
+   * COMPONENTS
+   ***************************************************************************/
+  components: { GDialogInput, ADocumentManager },
+}
+</script>
+
+<template>
+  <a-document-manager v-bind="$attrs">
+    <template #default="{ doc, dialog, triggers }">
+      <div>
+        <slot name="default" v-bind="{ attrs: doc, on: triggers }" />
+        <g-dialog-input v-bind="dialog.attrs" v-on="dialog.on">
+          <template #default="editorProps">
+            <slot name="editor" v-bind="editorProps" />
+          </template>
+        </g-dialog-input>
+      </div>
+    </template>
+  </a-document-manager>
+</template>
+
+<style></style>
