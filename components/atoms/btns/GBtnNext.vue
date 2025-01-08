326b26b6fd6a9e791613d0c8b9be76e43982b455
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-01-08 13:06:15 +0900

    add:
    - 共有ボタンコンポーネントを追加

---
+++ components/atoms/btns/GBtnNext.vue
@@ -0,0 +1,36 @@
+<script>
+/**
+ * 次へボタンコンポーネントです。
+ *
+ * @author shisyamo4131
+ */
+import GIconNext from '../icons/GIconNext.vue'
+import GBtn from './GBtn.vue'
+export default {
+  /***************************************************************************
+   * COMPONENTS
+   ***************************************************************************/
+  components: { GBtn, GIconNext },
+
+  /***************************************************************************
+   * PROPS
+   ***************************************************************************/
+  props: {
+    /**
+     * ボタンのラベルです。
+     */
+    label: { type: String, default: '次へ', required: false },
+  },
+}
+</script>
+
+<template>
+  <g-btn v-bind="{ ...$props, ...$attrs }" v-on="$listeners">
+    <template #icon="{ attrs }">
+      <g-icon-next v-bind="attrs" />
+    </template>
+    <span>{{ label }}</span>
+  </g-btn>
+</template>
+
+<style></style>
---
+++ components/atoms/btns/GBtnPrev.vue
@@ -0,0 +1,36 @@
+<script>
+/**
+ * 前へボタンコンポーネントです。
+ *
+ * @author shisyamo4131
+ */
+import GIconPrev from '../icons/GIconPrev.vue'
+import GBtn from './GBtn.vue'
+export default {
+  /***************************************************************************
+   * COMPONENTS
+   ***************************************************************************/
+  components: { GBtn, GIconPrev },
+
+  /***************************************************************************
+   * PROPS
+   ***************************************************************************/
+  props: {
+    /**
+     * ボタンのラベルです。
+     */
+    label: { type: String, default: '前へ', required: false },
+  },
+}
+</script>
+
+<template>
+  <g-btn v-bind="{ ...$props, ...$attrs }" v-on="$listeners">
+    <template #icon="{ attrs }">
+      <g-icon-prev v-bind="attrs" />
+    </template>
+    <span>{{ label }}</span>
+  </g-btn>
+</template>
+
+<style></style>
