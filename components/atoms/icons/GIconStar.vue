fd23d1b58b54447035cd09349ea46c84fc91ea31
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-01-08 16:12:15 +0900

    add:
    - 新規アイコン

---
+++ components/atoms/icons/GIconStar.vue
@@ -0,0 +1,15 @@
+<script>
+/**
+ * ★アイコンコンポーネントです。
+ * 主に資格者に関するマークとして使用されます。
+ * @author shisyamo4131
+ */
+
+export default {}
+</script>
+
+<template>
+  <v-icon v-bind="$attrs" v-on="$listeners">mdi-star</v-icon>
+</template>
+
+<style></style>
