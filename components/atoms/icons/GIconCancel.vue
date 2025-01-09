11c215da1f689361ea14b4cc311b31299b3eddb0
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-01-09 12:56:01 +0900

    refact: 不要なコメントを削除

--- components/atoms/icons/GIconCancel.vue
+++ components/atoms/icons/GIconCancel.vue
@@ -1,21 +1,9 @@
 <script>
 /**
- * ### GIconCancel
  * キャンセル操作を示すアイコンコンポーネントです。
- *
- * @version 1.0.0
- * @create 2024-06-18
  * @author shisyamo4131
  *
- * 概要:
- * GIconCancelコンポーネントは、Vuetifyの `v-icon` を使用してキャンセル操作を示すアイコンを表示します。
- * アイコンは常にmdi-closeを表示します。
- *
- * 使用例:
- * <GIconCancel />
- *
- * 更新履歴:
- * 2024-06-18 - 初版作成
+ * @refact 2025-01-09
  */
 
 export default {}
@@ -25,6 +13,4 @@ export default {}
   <v-icon v-bind="$attrs" v-on="$listeners">mdi-close</v-icon>
 </template>
 
-<style>
-/* 必要に応じてスタイルを追加 */
-</style>
+<style></style>
---
+++ components/atoms/icons/GIconPlay.vue
@@ -0,0 +1,16 @@
+<script>
+/**
+ * 稼働中であることを示すアイコンコンポーネントです。
+ * @author shisyamo4131
+ *
+ * @refact 2025-01-09
+ */
+
+export default {}
+</script>
+
+<template>
+  <v-icon v-bind="$attrs" v-on="$listeners">mdi-play</v-icon>
+</template>
+
+<style></style>
