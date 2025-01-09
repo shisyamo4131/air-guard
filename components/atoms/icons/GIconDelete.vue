9e4af8183a2919c29bc92fa3015fab646fa1e88e
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-01-09 17:09:10 +0900

    refact: コードを整理

--- components/atoms/icons/GIconDelete.vue
+++ components/atoms/icons/GIconDelete.vue
@@ -1,30 +1,16 @@
 <script>
 /**
- * ### GIconDelete
  * 削除操作を示すアイコンコンポーネントです。
- *
- * @version 1.0.0
- * @create 2024-06-18
  * @author shisyamo4131
  *
- * 概要:
- * GIconDeleteコンポーネントは、Vuetifyの `v-icon` を使用して削除操作を示すアイコンを表示します。
- * アイコンは常にmdi-deleteを表示します。
- *
- * 使用例:
- * <GIconDelete />
- *
- * 更新履歴:
- * 2024-06-18 - 初版作成
+ * @refact 2025-01-09
  */
 
 export default {}
 </script>
 
 <template>
-  <v-icon v-bind="{ ...$props, ...$attrs }" v-on="$listeners">
-    mdi-delete
-  </v-icon>
+  <v-icon v-bind="$attrs" v-on="$listeners"> mdi-delete </v-icon>
 </template>
 
 <style>
