6d35e70c55bda36e7218bb42ede3cf0a8053b1bf
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-01-20 15:09:39 +0900

    commit

---
+++ components/molecules/inputs/GInputCompanyInfoV2.vue
@@ -0,0 +1,95 @@
+<script>
+/**
+ * 自社情報入力コンポーネント
+ * @author shisyamo4131
+ * @refact 2025-01-20
+ */
+import GTextField from '../../atoms/inputs/GTextField.vue'
+import GTextFieldZipcode from '~/components/atoms/inputs/GTextFieldZipcode.vue'
+import GMixinEditModeReceiver from '~/mixins/GMixinEditModeReceiver'
+import { vueProps } from '~/models/propsDefinition/CompanyInfo'
+
+export default {
+  /***************************************************************************
+   * COMPONENTS
+   ***************************************************************************/
+  components: { GTextField, GTextFieldZipcode },
+
+  /***************************************************************************
+   * MIXINS
+   ***************************************************************************/
+  mixins: [GMixinEditModeReceiver],
+
+  /***************************************************************************
+   * PROPS
+   ***************************************************************************/
+  props: vueProps,
+}
+</script>
+
+<template>
+  <div>
+    <g-text-field
+      :value="name1"
+      label="会社名1"
+      required
+      @input="$emit('update:name1', $event)"
+    />
+    <g-text-field
+      :value="name2"
+      label="会社名2"
+      @input="$emit('update:name2', $event)"
+    />
+    <g-text-field-zipcode
+      :value="zipcode"
+      @input="$emit('update:zipcode', $event)"
+      @loaded="$emit('update:address1', $event.full)"
+    />
+    <g-text-field
+      :value="address1"
+      label="住所"
+      required
+      @input="$emit('update:address1', $event)"
+    />
+    <g-text-field
+      :value="address2"
+      label="建物名・階数"
+      @input="$emit('update:address2', $event)"
+    />
+    <v-row dense>
+      <v-col cols="12" sm="6">
+        <g-text-field
+          :value="tel"
+          label="電話番号"
+          input-type="tel"
+          @input="$emit('update:tel', $event)"
+        />
+      </v-col>
+      <v-col cols="12" sm="6">
+        <g-text-field
+          :value="fax"
+          label="FAX番号"
+          input-type="tel"
+          @input="$emit('update:fax', $event)"
+        />
+      </v-col>
+    </v-row>
+    <g-text-field
+      :value="corporateNumber"
+      label="法人番号"
+      @input="$emit('update:corporateNumber', $event)"
+    />
+    <g-text-field
+      :value="executiveName"
+      label="代表者名"
+      @input="$emit('update:executiveName', $event)"
+    />
+    <g-text-field
+      :value="executiveTitle"
+      label="代表者肩書"
+      @input="$emit('update:executiveTitle', $event)"
+    />
+  </div>
+</template>
+
+<style></style>
--- pages/settings/index.vue
+++ pages/settings/index.vue
@@ -3,12 +3,11 @@
  * 設定画面です。
  * @author shisyamo4131
  */
-import GDialogInput from '~/components/molecules/dialogs/GDialogInput.vue'
-import GTemplateDetail from '~/components/templates/GTemplateDetail.vue'
-import GMixinEditModeProvider from '~/mixins/GMixinEditModeProvider'
-import GInputCompanyInfo from '~/components/molecules/inputs/GInputCompanyInfo.vue'
 import CompanyInfo from '~/models/CompanyInfo'
-import GCardCompanyInfo from '~/components/molecules/cards/GCardCompanyInfo.vue'
+import AirItemManager from '~/components/air/AirItemManager.vue'
+import GBtnEdit from '~/components/atoms/btns/GBtnEdit.vue'
+import GInputCompanyInfoV2 from '~/components/molecules/inputs/GInputCompanyInfoV2.vue'
+import GTemplateDefault from '~/components/templates/GTemplateDefault.vue'
 export default {
   /***************************************************************************
    * NAME
@@ -19,42 +18,49 @@ export default {
    * COMPONENTS
    ***************************************************************************/
   components: {
-    GTemplateDetail,
-    GDialogInput,
-    GInputCompanyInfo,
-    GCardCompanyInfo,
+    AirItemManager,
+    GBtnEdit,
+    GInputCompanyInfoV2,
+    GTemplateDefault,
   },
 
   /***************************************************************************
-   * MIXINS
+   * ASYNCDATA
    ***************************************************************************/
-  mixins: [GMixinEditModeProvider],
+  asyncData({ app, route }) {
+    const instances = {
+      companyInfo: new CompanyInfo(),
+    }
+    return { instances }
+  },
 
   /***************************************************************************
    * DATA
    ***************************************************************************/
-  data() {
-    return {
-      /**
-       * ダイアログ制御
-       */
-      dialog: {
-        companyInfo: false,
-      },
-
-      /**
-       * 各種ドキュメントに対するリスナー
-       */
-      listener: {
-        companyInfo: new CompanyInfo(),
-      },
-    }
-  },
+  data() {},
 
   /***************************************************************************
    * COMPUTED
    ***************************************************************************/
-  computed: {},
+  computed: {
+    /**
+     * メイン情報として表示する項目の配列です。
+     */
+    mainProps() {
+      return [
+        { text: '会社名1', value: 'name1' },
+        { text: '会社名2', value: 'name2' },
+        { text: '郵便番号', value: 'zipcode' },
+        { text: '住所', value: 'address1' },
+        { text: '建物名・階数', value: 'address2' },
+        { text: '電話番号', value: 'tel' },
+        { text: 'FAX番号', value: 'fax' },
+        { text: '法人番号', value: 'corporateNumber' },
+        { text: '代表者名', value: 'executiveName' },
+        { text: '代表者肩書', value: 'executiveTitle' },
+      ]
+    },
+  },
 
   /***************************************************************************
    * WATCH
@@ -64,52 +70,65 @@ export default {
   /***************************************************************************
    * MOUNTED
    ***************************************************************************/
-  mounted() {
-    this.listener.companyInfo.subscribe('companyInfo')
-  },
+  mounted() {},
 
   /***************************************************************************
    * DESTROYED
    ***************************************************************************/
-  destroyed() {
-    if (this.listener.companyInfo) this.listener.companyInfo.unsubscribe()
-  },
+  destroyed() {},
 
   /***************************************************************************
    * METHODS
    ***************************************************************************/
   methods: {
-    /**
-     * 自社情報編集画面を開きます。
-     */
-    onClickEdit() {
-      this.dialog.companyInfo = true
+    async handleUpdate(item) {
+      await item.update()
     },
   },
 }
 </script>
 
 <template>
-  <g-template-detail
-    :actions="[{ event: 'edit', icon: 'mdi-pencil', color: 'green' }]"
-    @click:edit="onClickEdit"
-  >
+  <g-template-default>
     <!-- 自社情報 -->
     <v-container>
-      <g-card-company-info />
+      <air-item-manager
+        disable-delete
+        :dialog-props="{ maxWidth: 600 }"
+        :handle-update="handleUpdate"
+        :item="$store.state['company-info'].item"
+        label="自社情報"
+      >
+        <template #default="defaultProps">
+          <v-card>
+            <v-card-title>自社情報</v-card-title>
+            <v-list>
+              <v-list-item v-for="(prop, index) of mainProps" :key="index">
+                <v-list-item-content>
+                  <v-list-item-subtitle>
+                    {{ prop.text }}
+                  </v-list-item-subtitle>
+                  <v-list-item-title>
+                    {{ defaultProps.attrs[prop.value] }}
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+            <v-card-actions class="justify-end">
+              <g-btn-edit
+                :color="defaultProps.attrs.color"
+                icon
+                @click="defaultProps.on['click:edit']"
+              />
+            </v-card-actions>
+          </v-card>
+        </template>
+        <template #inputs="{ attrs, on }">
+          <g-input-company-info-v-2 v-bind="attrs" v-on="on" />
+        </template>
+      </air-item-manager>
     </v-container>
-
-    <!-- editor -->
-    <g-dialog-input
-      v-model="dialog.companyInfo"
-      :edit-mode="UPDATE"
-      :instance="listener.companyInfo"
-    >
-      <template #default="{ attrs, on }">
-        <g-input-company-info v-bind="attrs" v-on="on" />
-      </template>
-    </g-dialog-input>
-  </g-template-detail>
+  </g-template-default>
 </template>
 
 <style></style>
