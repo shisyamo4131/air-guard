1f6df0df71ea64a6419e0341d722e56489f3c752
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-01-20 17:55:20 +0900

    commit

--- components/molecules/inputs/GInputMedicalCheckup.vue
+++ components/molecules/inputs/GInputMedicalCheckup.vue
@@ -2,24 +2,23 @@
 /**
  * 従業員健康診断情報入力コンポーネント
  * @author shisayamo4131
+ * @refact 2025-01-20
  */
-import GCardInputForm from '../cards/GCardInputForm.vue'
 import GAutocompleteEmployee from './GAutocompleteEmployee.vue'
 import GTextField from '~/components/atoms/inputs/GTextField.vue'
-import GInputSubmitMixin from '~/mixins/GInputSubmitMixin'
-import MedicalCheckup from '~/models/MedicalCheckup'
 import GComboboxDate from '~/components/atoms/inputs/GComboboxDate.vue'
 import GSelect from '~/components/atoms/inputs/GSelect.vue'
 import GTextarea from '~/components/atoms/inputs/GTextarea.vue'
 import GNumeric from '~/components/atoms/inputs/GNumeric.vue'
 import GCheckbox from '~/components/atoms/inputs/GCheckbox.vue'
+import { vueProps } from '~/models/propsDefinition/MedicalCheckup'
+import GMixinEditModeReceiver from '~/mixins/GMixinEditModeReceiver'
 export default {
   /***************************************************************************
    * COMPONENTS
    ***************************************************************************/
   components: {
     GTextField,
-    GCardInputForm,
     GComboboxDate,
     GAutocompleteEmployee,
     GSelect,
@@ -29,82 +28,80 @@ export default {
   },
 
   /***************************************************************************
-   * MIXINS
+   * PROPS
    ***************************************************************************/
-  mixins: [GInputSubmitMixin],
+  mixins: [GMixinEditModeReceiver],
 
   /***************************************************************************
    * PROPS
    ***************************************************************************/
-  props: {
-    instance: {
-      type: Object,
-      required: true,
-      validator(instance) {
-        return instance instanceof MedicalCheckup
-      },
-    },
-  },
-
-  /***************************************************************************
-   * DATA
-   ***************************************************************************/
-  data() {
-    return {
-      editModel: new MedicalCheckup(),
-    }
-  },
+  props: vueProps,
 }
 </script>
 
 <template>
-  <g-card-input-form
-    v-bind="$attrs"
-    label="健康診断情報編集"
-    :edit-mode.sync="editMode"
-    :loading="loading"
-    @click:submit="submit"
-    v-on="$listeners"
-  >
+  <div>
     <v-row>
       <v-col cols="12" md="6">
         <g-autocomplete-employee
-          v-model="editModel.employeeId"
+          :value="employeeId"
           label="従業員"
           required
           :disabled="editMode !== CREATE"
+          @input="$emit('update:employeeId', $event)"
         />
-        <g-combobox-date v-model="editModel.date" label="受診日" required />
-        <g-text-field v-model="editModel.agency" label="受診機関" required />
+        <g-combobox-date
+          :value="date"
+          label="受診日"
+          required
+          @input="$emit('update:date', $event)"
+        />
+        <g-text-field
+          :value="agency"
+          label="受診機関"
+          required
+          @input="$emit('update:agency', $event)"
+        />
         <g-select
-          v-model="editModel.type"
+          :value="type"
           label="受診区分"
           :items="$MEDICAL_CHECKUP_TYPES_ARRAY"
           required
+          @input="$emit('update:type', $event)"
         />
         <v-row>
           <v-col cols="6">
             <g-numeric
-              v-model="editModel.bloodTop"
+              :value="bloodTop"
               label="血圧（上）"
               required
+              @input="$emit('update:bloodTop', $event)"
             />
           </v-col>
           <v-col cols="6">
             <g-numeric
-              v-model="editModel.bloodBottom"
+              :value="bloodBottom"
               label="血圧（下）"
               required
+              @input="$emit('update:bloodBottom', $event)"
             />
           </v-col>
         </v-row>
-        <g-checkbox v-model="editModel.hasFindings" label="所見" />
+        <g-checkbox
+          :input-value="hasFindings"
+          label="所見"
+          @change="$emit('update:hasFindings', $event)"
+        />
       </v-col>
       <v-col cols="12" md="6">
-        <g-textarea v-model="editModel.remarks" label="備考" />
+        <g-textarea
+          :value="remarks"
+          label="備考"
+          @input="$emit('update:remarks', $event)"
+        />
       </v-col>
     </v-row>
-  </g-card-input-form>
+  </div>
 </template>
 
 <style></style>
--- pages/medical-checkups/index.vue
+++ pages/medical-checkups/index.vue
@@ -1,8 +1,13 @@
 <script>
-import GDataTableMedicalCheckups from '~/components/atoms/tables/GDataTableMedicalCheckups.vue'
-import GAutocompleteEmployee from '~/components/molecules/inputs/GAutocompleteEmployee.vue'
+/**
+ * 健康診断情報の一覧ページです。
+ * @author shisyamo4131
+ * @refact 2025-01-20
+ */
+import AirArrayManager from '~/components/air/AirArrayManager.vue'
+import GBtnRegist from '~/components/atoms/btns/GBtnRegist.vue'
 import GInputMedicalCheckup from '~/components/molecules/inputs/GInputMedicalCheckup.vue'
-import GTemplateDocumentsIndex from '~/components/templates/GTemplateDocumentsIndex.vue'
+import GTemplateDefault from '~/components/templates/GTemplateDefault.vue'
 import MedicalCheckup from '~/models/MedicalCheckup'
 export default {
   /***************************************************************************
@@ -14,10 +19,10 @@ export default {
    * COMPONENTS
    ***************************************************************************/
   components: {
-    GAutocompleteEmployee,
+    GTemplateDefault,
     GInputMedicalCheckup,
-    GDataTableMedicalCheckups,
-    GTemplateDocumentsIndex,
+    AirArrayManager,
+    GBtnRegist,
   },
 
   /***************************************************************************
@@ -25,73 +30,103 @@ export default {
    ***************************************************************************/
   data() {
     return {
-      docs: [],
-      instance: new MedicalCheckup(),
-      selectedEmployeeId: null,
+      items: [],
+      schema: new MedicalCheckup(),
     }
   },
 
   /***************************************************************************
-   * WATCH
+   * MOUNTED
    ***************************************************************************/
-  watch: {
-    selectedEmployeeId: {
-      handler(v) {
-        this.subscribe()
-      },
-      immediate: true,
-    },
+  mounted() {
+    this.items = this.schema.subscribeDocs()
   },
 
   /***************************************************************************
    * DESTROYED
    ***************************************************************************/
   destroyed() {
-    this.unsubscribe()
+    this.schema.unsubscribe()
   },
 
   /***************************************************************************
    * METHODS
    ***************************************************************************/
   methods: {
-    /**
-     * 健康診断ドキュメントへの購読を開始します。
-     */
-    subscribe() {
-      this.unsubscribe()
-      if (!this.selectedEmployeeId) return
-      this.docs = this.instance.subscribeDocs([
-        ['where', 'employeeId', '==', this.selectedEmployeeId],
-      ])
+    async handleCreate(item) {
+      await item.create()
     },
-
-    /**
-     * 健康診断ドキュメントへの購読を解除します。
-     */
-    unsubscribe() {
-      this.docs.splice(0)
-      this.instance.unsubscribe()
+    async handleUpdate(item) {
+      await item.update()
+    },
+    async handleDelete(item) {
+      await item.delete()
     },
   },
 }
 </script>
 
 <template>
-  <g-template-documents-index
-    label="健康診断管理"
-    :items="docs"
-    :instance="instance"
-  >
-    <template #input="{ attrs, on }">
-      <g-input-medical-checkup v-bind="attrs" v-on="on" />
-    </template>
-    <template #search="{ attrs }">
-      <g-autocomplete-employee v-bind="attrs" v-model="selectedEmployeeId" />
-    </template>
-    <template #default="{ attrs, on }">
-      <g-data-table-medical-checkups v-bind="attrs" v-on="on" />
-    </template>
-  </g-template-documents-index>
+  <g-template-default v-slot="{ height }">
+    <v-container fluid :style="{ height: `${height}px` }">
+      <air-array-manager
+        :dialog-props="{
+          maxWidth: 600,
+        }"
+        event-edit="click:row"
+        :handle-create="handleCreate"
+        :handle-update="handleUpdate"
+        :handle-delete="handleDelete"
+        height="100%"
+        :items="items"
+        label="健康診断情報"
+        :schema="schema"
+      >
+        <template #default="props">
+          <v-sheet class="d-flex flex-column" :height="props.height">
+            <v-toolbar class="flex-grow-0" flat>
+              <v-text-field
+                v-bind="props.search.attrs"
+                v-on="props.search.on"
+              />
+              <g-btn-regist
+                v-bind="props.activator.attrs"
+                icon
+                v-on="props.activator.on"
+              />
+            </v-toolbar>
+            <div class="flex-table-container">
+              <v-data-table
+                v-bind="props.table.attrs"
+                :headers="[
+                  { text: 'CODE', value: 'employee.code' },
+                  { text: '従業員', value: 'employee.fullName' },
+                  { text: '受診日', value: 'date' },
+                  { text: '受診機関', value: 'agency' },
+                ]"
+                hide-default-footer
+                v-on="props.table.on"
+              >
+              </v-data-table>
+            </div>
+            <v-container fluid>
+              <v-row justify="center">
+                <v-col cols="10">
+                  <v-pagination
+                    v-bind="props.pagination.attrs"
+                    v-on="props.pagination.on"
+                  />
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-sheet>
+        </template>
+        <template #inputs="{ attrs, on }">
+          <g-input-medical-checkup v-bind="attrs" v-on="on" />
+        </template>
+      </air-array-manager>
+    </v-container>
+  </g-template-default>
 </template>
 
 <style></style>
