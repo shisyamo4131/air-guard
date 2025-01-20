f5d289d80fa59735a2a9bdf2b95f1c62622e324f
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-01-20 16:27:47 +0900

    commit

--- pages/holidays/index.vue
+++ pages/holidays/index.vue
@@ -1,11 +1,29 @@
 <script>
+/**
+ * 祝日情報の管理画面です。
+ * @author shisyamo4131
+ * @refact 2025-01-20
+ */
 import { database } from 'air-firebase'
 import { onValue, ref } from 'firebase/database'
-import GCalendar from '~/components/atoms/calendars/GCalendar.vue'
 import GTemplateDefault from '~/components/templates/GTemplateDefault.vue'
+import GCalendarV2 from '~/components/atoms/calendars/GCalendarV2.vue'
+import GBtnPrev from '~/components/atoms/btns/GBtnPrev.vue'
+import GBtnNext from '~/components/atoms/btns/GBtnNext.vue'
 export default {
+  /***************************************************************************
+   * NAME
+   ***************************************************************************/
   name: 'HolidaysIndex',
-  components: { GCalendar, GTemplateDefault },
+
+  /***************************************************************************
+   * COMPONENTS
+   ***************************************************************************/
+  components: { GTemplateDefault, GCalendarV2, GBtnPrev, GBtnNext },
+
+  /***************************************************************************
+   * DATA
+   ***************************************************************************/
   data() {
     return {
       data: null,
@@ -13,6 +31,10 @@ export default {
       listener: null,
     }
   },
+
+  /***************************************************************************
+   * COMPUTED
+   ***************************************************************************/
   computed: {
     events() {
       if (!this.data) return []
@@ -31,6 +53,10 @@ export default {
       return this.date.slice(0, 4)
     },
   },
+
+  /***************************************************************************
+   * WATCH
+   ***************************************************************************/
   watch: {
     month: {
       handler() {
@@ -39,18 +65,60 @@ export default {
       immediate: true,
     },
   },
+
+  /***************************************************************************
+   * DESTROYED
+   ***************************************************************************/
+  destroyed() {
+    this.unsubscribe()
+  },
+
+  /***************************************************************************
+   * METHODS
+   ***************************************************************************/
   methods: {
-    unsubscribe() {
-      if (this.listener) this.listener()
+    /**
+     * 基準日（date）を翌月月初にします。
+     */
+    next() {
+      this.date = this.$dayjs(this.date)
+        .startOf('month')
+        .add(1, 'month')
+        .format('YYYY-MM-DD')
     },
+
+    /**
+     * 基準日（date）を前月月初に設定します。
+     */
+    prev() {
+      this.date = this.$dayjs(this.date)
+        .startOf('month')
+        .subtract(1, 'month')
+        .format('YYYY-MM-DD')
+    },
+
+    /**
+     * 祝日データの購読を開始します。
+     */
     subscribe() {
       const dbRef = ref(database, `Holidays/${this.year}/${this.month}`)
       this.listener = onValue(dbRef, (snapshot) => {
         this.data = snapshot.val()
       })
     },
+
+    /**
+     * 祝日データを更新します。
+     */
     async refresh() {
       await this.$holiday.fetchAndSaveHolidays(this.year)
+    },
+
+    /**
+     * 祝日データの購読を解除します。
+     */
+    unsubscribe() {
+      if (this.listener) this.listener()
     },
   },
 }
@@ -65,7 +133,7 @@ export default {
         class="d-flex flex-column"
         :tile="$vuetify.breakpoint.mobile"
       >
-        <v-toolbar class="flex-grow-0" color="secondary" dark dense flat>
+        <v-toolbar class="flex-grow-0" color="primary" dark dense flat>
           <v-icon left>mdi-flag</v-icon>
           <v-toolbar-title>祝日設定</v-toolbar-title>
           <v-spacer />
@@ -73,8 +141,15 @@ export default {
             <v-btn text @click="refresh">更新</v-btn>
           </v-toolbar-items>
         </v-toolbar>
-        <v-card-text class="pa-0 pa-md-4 flex-grow-1">
-          <g-calendar v-model="date" style="height: 100%" :events="events" />
+        <v-toolbar class="flex-grow-0" flat>
+          <v-spacer />
+          <g-btn-prev color="primary" icon @click="prev" />
+          <span class="text-h6">{{ month }}</span>
+          <g-btn-next color="primary" icon @click="next" />
+          <v-spacer />
+        </v-toolbar>
+        <v-card-text class="pa-0 pa-md-4 pt-0 flex-grow-1">
+          <g-calendar-v-2 v-model="date" :events="events" />
         </v-card-text>
       </v-card>
     </v-container>
