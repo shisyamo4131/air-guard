2e59ec528c3d21d6b5c3dbcf295390c814f0b5ae
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-01-20 16:16:27 +0900

    commit

---
+++ components/molecules/inputs/GInputNewUserV2.vue
@@ -0,0 +1,60 @@
+<script>
+/**
+ * NewUsersドキュメント入力コンポーネント
+ * @author shisyamo4131
+ * @refact 2025-01-20
+ */
+import GTextField from '../../atoms/inputs/GTextField.vue'
+import GAutocompleteEmployee from '~/components/molecules/inputs/GAutocompleteEmployee.vue'
+import { vueProps } from '~/models/propsDefinition/NewUser'
+import GMixinEditModeReceiver from '~/mixins/GMixinEditModeReceiver'
+
+export default {
+  /***************************************************************************
+   * COMPONENTS
+   ***************************************************************************/
+  components: { GTextField, GAutocompleteEmployee },
+
+  /***************************************************************************
+   * MIXINS
+   ***************************************************************************/
+  mixins: [GMixinEditModeReceiver],
+
+  /***************************************************************************
+   * PROPS
+   ***************************************************************************/
+  props: vueProps,
+}
+</script>
+
+<template>
+  <div>
+    <g-text-field
+      :value="email"
+      label="email"
+      required
+      input-type="email"
+      @input="$emit('update:email', $event)"
+    />
+    <g-text-field
+      :value="password"
+      label="password"
+      required
+      type="password"
+      @input="$emit('update:password', $event)"
+    />
+    <g-autocomplete-employee
+      :value="employeeId"
+      label="従業員"
+      @input="$emit('update:employeeId', $event)"
+    />
+    <g-text-field
+      :value="displayName"
+      label="表示名"
+      required
+      @input="$emit('update:displayName', $event)"
+    />
+  </div>
+</template>
+
+<style></style>
--- pages/admin/users/index.vue
+++ pages/admin/users/index.vue
@@ -1,24 +1,18 @@
 <script>
 /**
- * ## pages.UsersIndex
- *
  * ユーザー情報の一覧ページです。
- * - 新規登録時は、Authentications でユーザーアカウントを同期作成するために
- *   NewUser クラスを使ってドキュメントを作成します。
- * - 編集時は User クラスを使ってドキュメントの内容を更新します。
- *
  * @author shisyamo4131
+ * @refact 2025-01-20
  */
-import GInputUser from '~/components/molecules/inputs/GInputUser.vue'
-import GDataTableUsers from '~/components/molecules/tables/GDataTableUsers.vue'
-import User from '~/models/User'
-import GDialogInput from '~/components/molecules/dialogs/GDialogInput.vue'
-import NewUser from '~/models/NewUser'
-import GInputNewUser from '~/components/molecules/inputs/GInputNewUser.vue'
-import GBtnSubmit from '~/components/atoms/btns/GBtnSubmit.vue'
+import AirArrayManager from '~/components/air/AirArrayManager.vue'
 import GBtnCancel from '~/components/atoms/btns/GBtnCancel.vue'
-import GTemplateDocumentsIndex from '~/components/templates/GTemplateDocumentsIndex.vue'
 import GBtnRegist from '~/components/atoms/btns/GBtnRegist.vue'
+import GBtnSubmit from '~/components/atoms/btns/GBtnSubmit.vue'
+import GInputNewUserV2 from '~/components/molecules/inputs/GInputNewUserV2.vue'
+import GInputUserV2 from '~/components/molecules/inputs/GInputUserV2.vue'
+import GTemplateDefault from '~/components/templates/GTemplateDefault.vue'
+import NewUser from '~/models/NewUser'
+import User from '~/models/User'
 export default {
   /***************************************************************************
    * NAME
@@ -29,14 +23,13 @@ export default {
    * COMPONENTS
    ***************************************************************************/
   components: {
-    GInputUser,
-    GDataTableUsers,
-    GDialogInput,
-    GInputNewUser,
-    GBtnSubmit,
-    GBtnCancel,
-    GTemplateDocumentsIndex,
+    GTemplateDefault,
+    AirArrayManager,
     GBtnRegist,
+    GInputUserV2,
+    GInputNewUserV2,
+    GBtnCancel,
+    GBtnSubmit,
   },
 
   /***************************************************************************
@@ -44,17 +37,13 @@ export default {
    ***************************************************************************/
   data() {
     return {
-      instance: new User(),
-      regist: {
-        dialog: false,
-        editMode: 'CREATE',
-        instance: new NewUser(),
-      },
       restore: {
         dialog: false,
         loading: false,
         uid: null,
       },
+      schema: new User(),
+      schemaNewUser: new NewUser(),
     }
   },
 
@@ -75,16 +64,6 @@ export default {
      * data.restore.dialog を監視します。
      * - false に更新されたら data.restore.uid を null にします。
      */
-    'regist.dialog'(v) {
-      if (v) return
-      this.regist.editMode = 'CREATE'
-      this.regist.instance.initialize()
-    },
-
-    /**
-     * data.restore.dialog を監視します。
-     * - false に更新されたら data.restore.uid を null にします。
-     */
     'restore.dialog'(v) {
       if (v) return
       this.restore.uid = null
@@ -95,6 +74,16 @@ export default {
    * METHODS
    ***************************************************************************/
   methods: {
+    async handleCreate(item) {
+      await item.create()
+    },
+    async handleUpdate(item) {
+      await item.update()
+    },
+    async handleDelete(item) {
+      await item.delete()
+    },
+
     /**
      * ユーザーアカウントの復元処理を実行します。
      */
@@ -119,69 +108,113 @@ export default {
 </script>
 
 <template>
-  <g-template-documents-index
-    label="ユーザー管理"
-    :items="items"
-    :instance="instance"
-  >
-    <template #regist-button="buttonProps">
-      <g-dialog-input
-        v-model="regist.dialog"
-        :edit-mode.sync="regist.editMode"
-        :instance="regist.instance"
+  <g-template-default v-slot="{ height }">
+    <v-container fluid :style="{ height: `${height}px` }">
+      <air-array-manager
+        :dialog-props="{
+          maxWidth: 600,
+        }"
+        event-edit="click:row"
+        :handle-update="handleUpdate"
+        :handle-delete="handleDelete"
+        height="100%"
+        :items="items"
+        label="ユーザー情報"
+        :schema="schema"
       >
-        <template #activator="{ attrs, on }">
-          <g-btn-regist
-            icon
-            v-bind="{ ...buttonProps.attrs, ...attrs }"
-            v-on="on"
-          />
+        <template #default="props">
+          <v-sheet class="d-flex flex-column" :height="props.height">
+            <v-toolbar class="flex-grow-0" flat>
+              <v-text-field
+                v-bind="props.search.attrs"
+                v-on="props.search.on"
+              />
+              <air-array-manager
+                :dialog-props="{
+                  maxWidth: 600,
+                }"
+                :handle-create="handleCreate"
+                label="ユーザー情報"
+                :schema="schemaNewUser"
+              >
+                <template #default="{ activator }">
+                  <g-btn-regist
+                    v-bind="activator.attrs"
+                    icon
+                    v-on="activator.on"
+                  />
+                </template>
+                <template #inputs="{ attrs, on }">
+                  <g-input-new-user-v-2 v-bind="attrs" v-on="on" />
+                </template>
+              </air-array-manager>
+              <!-- 復元処理ダイアログ -->
+              <v-dialog v-model="restore.dialog" max-width="360">
+                <template #activator="{ attrs, on }">
+                  <v-btn v-bind="attrs" color="primary" icon v-on="on">
+                    <v-icon>mdi-delete-restore</v-icon>
+                  </v-btn>
+                </template>
+                <v-card>
+                  <v-toolbar color="primary" dark dense flat>
+                    <v-toolbar-title>アカウント復元処理</v-toolbar-title>
+                    <v-spacer />
+                    <g-btn-cancel
+                      icon
+                      :disabled="restore.loading"
+                      @click="restore.dialog = false"
+                    />
+                  </v-toolbar>
+                  <v-card-text class="pt-5">
+                    <v-text-field
+                      v-model="restore.uid"
+                      label="uid"
+                      hide-details
+                    />
+                  </v-card-text>
+                  <v-card-actions class="justify-end">
+                    <g-btn-submit
+                      icon
+                      color="primary"
+                      :disabled="!restore.uid || restore.loading"
+                      :loading="restore.loading"
+                      @click="onClickRestore"
+                    />
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </v-toolbar>
+            <div class="flex-table-container">
+              <v-data-table
+                v-bind="props.table.attrs"
+                :headers="[
+                  { text: '表示名', value: 'displayName' },
+                  { text: 'email', value: 'email' },
+                  { text: 'uid', value: 'docId' },
+                ]"
+                hide-default-footer
+                v-on="props.table.on"
+              >
+              </v-data-table>
+            </div>
+            <v-container fluid>
+              <v-row justify="center">
+                <v-col cols="10">
+                  <v-pagination
+                    v-bind="props.pagination.attrs"
+                    v-on="props.pagination.on"
+                  />
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-sheet>
         </template>
-        <template #default="{ attrs, on }">
-          <g-input-new-user v-bind="attrs" v-on="on" />
+        <template #inputs="{ attrs, on }">
+          <g-input-user-v-2 v-bind="attrs" v-on="on" />
         </template>
-      </g-dialog-input>
-    </template>
-    <template #input="{ attrs, on }">
-      <g-input-user v-bind="attrs" v-on="on" />
-    </template>
-    <template #append-search>
-      <!-- 復元処理ダイアログ -->
-      <v-dialog v-model="restore.dialog" max-width="360">
-        <template #activator="{ attrs, on }">
-          <v-btn v-bind="attrs" color="secondary" icon v-on="on">
-            <v-icon>mdi-delete-restore</v-icon>
-          </v-btn>
-        </template>
-        <v-card>
-          <v-toolbar color="secondary" dark dense flat>
-            <v-toolbar-title>アカウント復元処理</v-toolbar-title>
-            <v-spacer />
-            <g-btn-cancel
-              icon
-              :disabled="restore.loading"
-              @click="restore.dialog = false"
-            />
-          </v-toolbar>
-          <v-card-text class="pt-5">
-            <v-text-field v-model="restore.uid" label="uid" hide-details />
-          </v-card-text>
-          <v-card-actions class="justify-end">
-            <g-btn-submit
-              icon
-              color="primary"
-              :disabled="!restore.uid || restore.loading"
-              :loading="restore.loading"
-              @click="onClickRestore"
-            />
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </template>
-    <template #default="{ attrs, on }">
-      <g-data-table-users v-bind="attrs" sort-by="code" sort-desc v-on="on" />
-    </template>
-  </g-template-documents-index>
+      </air-array-manager>
+    </v-container>
+  </g-template-default>
 </template>
 
 <style></style>
