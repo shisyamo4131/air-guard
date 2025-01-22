1a48e28dcf94e4b62e99d19e84d16772f988b92b
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-01-22 10:50:28 +0900

    refact: コメントを追加

--- models/propsDefinition/LeaveRecord.js
+++ models/propsDefinition/LeaveRecord.js
@@ -1,23 +1,34 @@
+/**
+ * 休暇実績データモデル
+ */
 import { generateVueProps, generateClassProps } from './propsUtil'
 
 const propsDefinition = {
+  // ドキュメントID
   docId: { type: String, default: '', required: false, requiredByClass: false },
+
+  // 従業員ID
   employeeId: {
     type: String,
     default: '',
     required: false,
     requiredByClass: true,
   },
+
+  // 日付
   date: {
     type: String,
     default: '',
     required: false,
     requiredByClass: true,
   },
+
+  // 休暇種別
   leaveType: {
     type: String,
     default: '',
     validator: (v) =>
+      !v ||
       [
         'absent', // 欠勤
         'substitute', // 振替休日
@@ -27,37 +38,50 @@ const propsDefinition = {
     required: false,
     requiredByClass: true,
   },
+
+  // 振替出勤日
   substituteWorkDate: {
     type: String,
     default: '',
     required: false,
     requiredByClass: false,
   },
+
+  // 振替出勤日区分
   substitutedDayType: {
     type: String,
     default: '',
-    validator: (v) => ['non-statutory-holiday', 'legal-holiday'].includes(v),
+    validator: (v) =>
+      !v || ['non-statutory-holiday', 'legal-holiday'].includes(v),
     required: false,
     requiredByClass: false,
   },
+
+  // 年次有給休暇（休暇を選択された場合に年次有給とするかどうか）
   isAnnualPaidLeave: {
     type: Boolean,
     default: false,
     required: false,
     requiredByClass: false,
   },
+
+  // 有給休暇（休暇を選択された場合に年次ではない有給とするかどうか）
   isPaidLeave: {
     type: Boolean,
     default: false,
     required: false,
     requiredByClass: false,
   },
+
+  // 有給休暇である場合の支給率
   leavePaymentRate: {
     type: Number,
     default: 0,
     required: false,
     requiredByClass: true,
   },
+
+  // 備考
   remarks: {
     type: String,
     default: '',
