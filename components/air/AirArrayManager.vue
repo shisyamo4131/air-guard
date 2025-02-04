2aa4d9a5f5bc2c576601227a52ddc5618e615730
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-02-04 14:22:28 +0900

    change: searchSlotProps に clearable を追加

--- components/air/AirArrayManager.vue
+++ components/air/AirArrayManager.vue
@@ -479,6 +479,7 @@ export default {
     searchSlotProps() {
       return {
         attrs: {
+          clearable: true,
           hideDetails: true,
           placeholder: 'SEARCH',
           prependInnerIcon: 'mdi-magnify',
