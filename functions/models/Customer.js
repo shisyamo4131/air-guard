bc07977b616709da7ff93a9653f4e2dad0d33ae4
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-02-06 13:32:16 +0900

    refact

--- functions/models/Customer.js
+++ functions/models/Customer.js
@@ -1,3 +1,9 @@
+/*****************************************************************************
+ * カスタムクラス定義: 取引先 - Customer -
+ *
+ * @author shisyamo4131
+ * @refact 2025-02-06
+ *****************************************************************************/
 import { logger } from 'firebase-functions/v2'
 import {
   createIndex,
@@ -5,40 +11,155 @@ import {
   syncToFirestoreFromAirGuard,
 } from '../modules/database.js'
 import FireModel from './FireModel.js'
-import { classProps } from './propsDefinition/Customer.js'
+import { generateProps } from './propsDefinition/propsUtil.js'
 
 /**
- * Cloud Functions で Firestore の Customers ドキュメントを操作するためのクラスです。
- * FireMode を継承していますが、更新系のメソッドは利用できません。
- * @author shisyamo4131
+ * 定数定義: 取引先状態
  */
+const CUSTOMER_STATUS = Object.freeze({
+  active: '取引中',
+  expired: '取引停止',
+})
+
+/**
+ * PROPERTIES
+ */
+const propsDefinition = {
+  // ドキュメントID
+  docId: { type: String, default: '', required: false },
+
+  // 取引先code
+  code: { type: String, default: '', required: false },
+
+  // 取引先名1
+  name1: { type: String, default: '', required: false, requiredByClass: true },
+
+  // 取引先名2
+  name2: { type: String, default: '', required: false },
+
+  // 取引先名略称
+  abbr: { type: String, default: '', required: false, requiredByClass: true },
+
+  // 取引先名略称カナ
+  abbrKana: {
+    type: String,
+    default: '',
+    required: false,
+    requiredByClass: true,
+  },
+
+  // 郵便番号
+  zipcode: {
+    type: String,
+    default: '',
+    required: false,
+  },
+
+  // 住所
+  address1: {
+    type: String,
+    default: '',
+    required: false,
+    requiredByClass: true,
+  },
+
+  // 建物名・階数
+  address2: {
+    type: String,
+    default: '',
+    required: false,
+  },
+
+  // 電話番号
+  tel: { type: String, default: '', required: false },
+
+  // FAX番号
+  fax: { type: String, default: '', required: false },
+
+  // 状態
+  status: {
+    type: String,
+    default: 'active',
+    validator: (v) => Object.keys(CUSTOMER_STATUS).includes(v),
+    required: false,
+    requiredByClass: true,
+  },
+
+  // 締日
+  deadline: {
+    type: String,
+    default: '99',
+    validator: (v) => ['05', '10', '15', '20', '25', '99'].includes(v),
+    required: false,
+    requiredByClass: true,
+  },
+
+  // 入金サイト（月）
+  depositMonth: {
+    type: Number,
+    default: 1,
+    validator: (v) => v > 0,
+    required: false,
+    requiredByClass: true,
+  },
+
+  // 入金サイト（日）
+  depositDate: {
+    type: String,
+    default: '99',
+    validator: (v) => ['05', '10', '15', '20', '25', '99'].includes(v),
+    required: false,
+    requiredByClass: true,
+  },
+
+  // 備考
+  remarks: {
+    type: String,
+    default: '',
+    required: false,
+  },
+
+  // 同期状態
+  sync: {
+    type: Boolean,
+    default: false,
+    required: false,
+  },
+
+  // 自社情報フラグ
+  isInternal: {
+    type: Boolean,
+    default: false,
+    required: false,
+  },
+}
+
+const { classProps } = generateProps(propsDefinition)
+
+/*****************************************************************************
+ * カスタムクラス - default -
+ *****************************************************************************/
 export default class Customer extends FireModel {
-  /****************************************************************************
-   * STATIC
-   ****************************************************************************/
+  // FireModel 設定
   static collectionPath = 'Customers'
+  static useAutonumber = true
+  static logicalDelete = true
   static classProps = classProps
+  static tokenFields = ['abbr', 'abbrKana']
+  static hasMany = [
+    {
+      collection: 'Sites',
+      field: 'customerId',
+      condition: '==',
+      type: 'collection',
+    },
+  ]
 
-  /****************************************************************************
-   * 更新系メソッドは使用不可
-   ****************************************************************************/
-  create() {
-    return Promise.reject(new Error('このクラスの create は使用できません。'))
-  }
-
-  update() {
-    return Promise.reject(new Error('このクラスの update は使用できません。'))
-  }
-
-  delete() {
-    return Promise.reject(new Error('このクラスの delete は使用できません。'))
-  }
-
-  /****************************************************************************
+  /**
    * Realtime Databaseの`AirGuard/Customers`の内容で、FirestoreのCustomersドキュメントを更新します。
    * @param {string} code - Realtime Database内のCustomersデータを識別するコード
    * @returns {Promise<void>} - 同期が正常に完了した場合は、解決されたPromiseを返します
-   ****************************************************************************/
+   */
   static async syncFromAirGuard(code) {
     try {
       const converter = (item) => {
@@ -52,39 +173,75 @@ export default class Customer extends FireModel {
   }
 }
 
-/**
- * Realtime Database で管理する Customer インデックス用のクラスです。
- */
-export class CustomerIndex extends Customer {
-  /****************************************************************************
-   * CONSTRUCTOR
-   ****************************************************************************/
-  constructor(item = {}) {
-    super(item)
+/*****************************************************************************
+ * カスタムクラス - Minimal -
+ *****************************************************************************/
+export class CustomerMinimal extends Customer {
+  // initialize をオーバーライド
+  initialize(item = {}) {
+    super.initialize(item)
+    delete this.createAt
+    delete this.updateAt
+    delete this.uid
+    delete this.remarks
+    delete this.tokenMap
+  }
 
-    // インデックスに必要なプロパティを定義
-    const keepProps = [
-      'code',
-      'name1',
-      'name2',
-      'abbr',
-      'abbrKana',
-      'address1',
-      'status',
-      'sync',
-      'inInternal',
-    ]
+  // 更新系メソッドは使用不可
+  create() {
+    return Promise.reject(new Error('このクラスの create は使用できません。'))
+  }
 
-    // Customer クラスから不要なプロパティを削除
+  update() {
+    return Promise.reject(new Error('このクラスの update は使用できません。'))
+  }
+
+  delete() {
+    return Promise.reject(new Error('このクラスの delete は使用できません。'))
+  }
+
+  deleteAll() {
+    return Promise.reject(
+      new Error('このクラスの deleteAll は使用できません。')
+    )
+  }
+
+  restore() {
+    return Promise.reject(new Error('このクラスの restore は使用できません。'))
+  }
+}
+
+/*****************************************************************************
+ * カスタムクラス - Index -
+ *****************************************************************************/
+export class CustomerIndex extends CustomerMinimal {
+  // インデックスとして用意するプロパティを定義
+  static keepProps = [
+    'code',
+    'name1',
+    'name2',
+    'abbr',
+    'abbrKana',
+    'address1',
+    'status',
+    'sync',
+    'inInternal',
+  ]
+
+  // initialize をオーバーライド
+  initialize(item = {}) {
+    super.initialize(item)
+
+    // keepProps で定義されたプロパティ以外を削除
     Object.keys(this).forEach((key) => {
-      if (!keepProps.includes(key)) delete this[key]
+      if (!this.constructor.keepProps.includes(key)) delete this[key]
     })
   }
 
-  /****************************************************************************
+  /**
    * Realtime Database にインデックスを作成します。
    * @param {string} customerId - インデックス作成対象の取引先ID
-   ****************************************************************************/
+   */
   static async create(customerId) {
     const functionName = 'create'
     try {
@@ -95,10 +252,10 @@ export class CustomerIndex extends Customer {
     }
   }
 
-  /****************************************************************************
+  /**
    * Realtime Database からインデックスを削除します。
    * @param {string} customerId - インデックス削除対象の取引先ID
-   ****************************************************************************/
+   */
   static async remove(customerId) {
     const functionName = 'remove'
     try {
@@ -107,22 +264,5 @@ export class CustomerIndex extends Customer {
       logger.error(`[${functionName}] ${error.message}`, { customerId })
       throw error
     }
-  }
-}
-
-/**
- * Customer クラスからカスタムクラス用に不要なプロパティを削除したクラスです。
- */
-export class CustomerMinimal extends Customer {
-  /****************************************************************************
-   * CONSTRUCTOR
-   ****************************************************************************/
-  constructor(item = {}) {
-    super(item)
-
-    delete this.createAt
-    delete this.updateAt
-    delete this.remarks
-    delete this.tokenMap
   }
 }
