1a4485b5f801a4471048ab8be0433bc151bb17d3
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-02-12 19:26:54 +0900

    commit

--- components/air/AirDialogSelector.vue
+++ components/air/AirDialogSelector.vue
@@ -1,17 +1,35 @@
 <script>
 /**
- * 複数のアイテムからユーザーに単一または複数のアイテムを選択してもらうための
- * ダイアログ UI を提供するコンポーネントです。
+ * 複数のアイテムから単一または複数のアイテムを選択するためのダイアログコンポーネントです。
  *
- * - activator スロットを使用してダイアログを起動してください。
- *   または open を実行することでも起動が可能です。
- * - 選択肢となるアイテムの配列を props.items で受け取ります。
- * - item スロットで単一アイテムの表示を行います。
- * - props.fetcher を指定することで外部データの取得にも対応します。
- * - ダイアログの高さはアプリケーションの高さの 90% で固定されます。
+ * [特徴]
+ * - アイテム群の表示をダイアログで行う為、ダイアログコンポーネントの高さは
+ *   アプリケーションの高さの 90％ で固定されます。
+ *   但し、モバイル表示の場合はフルスクリーン表示になります。
+ *
+ * [使い方]
+ * 1. ダイアログの起動には activator スロットが使用可能です。
+ * 2. props.items に配列を渡し、初期表示するアイテム群を指定します。（オプション）
+ *    item スロットで提供されるプロパティを使用してアイテムを表示してください。
+ * 3. props.fetcher を指定すると検索のためのツールバーが表示されます。
+ *    検索文字列が入力されると props.fetcher の実行結果で取得した配列が
+ *    props.items に代わって提供されます。
+ * 4. アイテムが選択され、確定ボタンがクリックされると、選択されたアイテムが
+ *    selected イベントで emit されます。
+ *
+ * [fetcherParams]
+ * props.fetcher の第2引数として引き渡されるオブジェクトです。
+ * 親コンポーネントから追加でパラメータを指定する必要がある場合に使用します。
+ * このオブジェクトは watcher によって監視されており、内容の変更を検知すると
+ * props.fetcher が再実行されます。
+ *
+ * [customFilter]
+ * props.items または props.fetcher の実行結果で取得した配列をフィルタリングするための
+ * 関数です。
+ * customFilter の結果で得られる配列が、items または item スロットで提供されます。
  *
  * @author shisyamo4131
- * @refact 2025-02-11
+ * @refact 2025-02-12
  */
 import AirRenderlessDelayInput from '~/components/air/AirRenderlessDelayInput.vue'
 export default {
@@ -37,6 +55,18 @@ export default {
     },
 
     /**
+     * items または fetcher によって抽出されたアイテムをフィルタリングするための関数です。
+     * - コンポーネント内部ではすべてのアイテムを保有しますが、
+     *   UI の為に提供されるアイテムはこの関数でフィルタリングされます。
+     * (items, search) => []
+     */
+    customFilter: {
+      type: Function,
+      default: (items, search) => items,
+      required: false,
+    },
+
+    /**
      * 検索文字列として入力された値で fetcher を実行するまでの遅延時間（ミリ秒）です。
      */
     delay: { type: [String, Number], default: 500, required: false },
@@ -46,9 +76,15 @@ export default {
      * - fetcher が指定されると、コンポーネントに検索窓が表示されます。
      * - fetcher は検索文字列が更新されるたびに実行されます。
      * - fetcher が実行されると、選択可能なアイテムは props.items より優先されます。
-     * (search) => Promise<Array>
+     * (search, fetchParams) => Promise<Array>
      */
     fetcher: { type: Function, default: undefined, required: false },
+
+    /**
+     * fetcher の第二引数として引き渡されるパラメータです。
+     * - watcher によって内容が監視され、変更が生じるとアイテムの再クエリを行います。
+     */
+    fetchParams: { type: Object, default: () => ({}), required: false },
 
     /**
      * 選択可能なアイテムの配列です。
@@ -113,11 +149,12 @@ export default {
      * 並び替えた配列を返します。
      */
     computedItems() {
-      // sortBy の指定がなければ data.internalItems をそのまま返す。
+      // sortBy の指定がなければ filterdItems をそのまま返す。
       if (!this.sortBy || this.sortBy.length === 0) {
         return this.internalItems
       }
 
+      // sortBy の指定があれば、条件に従って配列を並び替えて返す。
       return this.internalItems.slice().sort((a, b) => {
         if (Array.isArray(this.sortBy)) {
           for (let i = 0; i < this.sortBy.length; i++) {
@@ -142,6 +179,10 @@ export default {
       })
     },
 
+    filterdItems() {
+      return this.customFilter(this.computedItems, this.lazySearch)
+    },
+
     /**
      * アイテムが選択されているかどうかを返します。
      */
@@ -157,9 +198,23 @@ export default {
    * WATCH
    ***************************************************************************/
   watch: {
-    // data.internalItems を監視し、値が更新されたらコンポーネントを初期化します。
+    /**
+     * props.fetchParams を監視し、内容に変更が生じたら reloadItems を実行します。
+     */
+    fetchParams: {
+      handler(v) {
+        this.reloadItems()
+      },
+      deep: true,
+    },
+
+    /**
+     * data.internalItems を監視し、値が更新されたらコンポーネントを初期化します。
+     * - `reloaded` イベントを emit します。
+     */
     internalItems(v) {
       this.initializeSelection()
+      this.$emit('reloaded')
     },
 
     /**
@@ -232,7 +287,7 @@ export default {
       this.loading = true
       try {
         this.internalItems = this.internalLazySearch
-          ? await this.fetcher(this.internalLazySearch)
+          ? await this.fetcher(this.internalLazySearch, this.fetchParams)
           : this.items
       } catch (err) {
         // eslint-disable-next-line no-console
@@ -281,9 +336,9 @@ export default {
         >
       </v-toolbar>
 
-      <!-- prepend-search スロット -->
-      <div class="px-6 flex-grow-0">
-        <slot name="prepend-search" />
+      <!-- header スロット -->
+      <div class="flex-grow-0">
+        <slot name="header" v-bind="{ items }" />
       </div>
 
       <div v-if="!!fetcher" class="px-6 flex-grow-0">
@@ -304,13 +359,13 @@ export default {
         </air-renderless-delay-input>
       </div>
 
-      <!-- append-search スロット -->
-      <div class="px-6 flex-grow-0">
-        <slot name="append-search" />
+      <!-- search-extention スロット -->
+      <div class="flex-grow-0">
+        <slot name="search-extension" v-bind="{ items: computedItems }" />
       </div>
 
-      <v-container class="py-0 flex-grow-1 overflow-y-hidden" fluid>
-        <slot name="items" v-bind="{ items: computedItems }">
+      <v-container class="flex-grow-1 overflow-y-hidden" fluid>
+        <slot name="items" v-bind="{ items: filterdItems }">
           <v-item-group
             ref="scroll-container"
             v-model="selectedItem"
@@ -321,7 +376,7 @@ export default {
             <v-container>
               <v-row>
                 <v-col
-                  v-for="(item, index) of computedItems"
+                  v-for="(item, index) of filterdItems"
                   :key="index"
                   v-bind="colOptions"
                 >
